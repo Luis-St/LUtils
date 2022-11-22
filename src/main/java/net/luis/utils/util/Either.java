1ef10ac8a655c7338c96b55c0361eb82e5d6317f
Luis <76595940+Luis-st@users.noreply.github.com>
2022-11-22 20:44:52 +0100

    added Either

---
+++ src/main/java/net/luis/utils/util/Either.java
@@ -0,0 +1,203 @@
+package net.luis.utils.util;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ *
+ * @author Luis-st
+ *
+ */
+
+public abstract class Either<L, R> {
+	
+	private Either() {
+		
+	}
+	
+	public static <L, R> Either<L, R> left(L value) {
+		return new Left<>(value);
+	}
+	
+	public static <L, R> Either<L, R> right(R value) {
+		return new Right<>(value);
+	}
+	
+	public abstract <C, D> Either<C, D> mapBoth(Function<? super L, ? extends C> leftFunction, Function<? super R, ? extends D> rightFunction);
+	
+	public abstract <T> T map(Function<? super L, ? extends T> leftFunction, Function<? super R, ? extends T> rightFunction);
+	
+	public abstract boolean isLeft();
+	
+	public abstract boolean isRight();
+	
+	public abstract Either<L, R> ifLeft(Consumer<? super L> consumer);
+	
+	public abstract Either<L, R> ifRight(Consumer<? super R> consumer);
+	
+	public abstract Optional<L> left();
+	
+	public abstract Optional<R> right();
+	
+	public <T> Either<T, R> mapLeft(Function<? super L, ? extends T> function) {
+		return this.map((value) -> {
+			return Either.left(function.apply(value));
+		}, Either::right);
+	}
+	
+	public <T> Either<L, T> mapRight(Function<? super R, ? extends T> function) {
+		return this.map(Either::left, (value) -> {
+			return Either.right(function.apply(value));
+		});
+	}
+	
+	public L leftOrThrow() {
+		return this.left().orElseThrow();
+	}
+	
+	public R rightOrThrow() {
+		return this.right().orElseThrow();
+	}
+	
+	public Either<R, L> swap() {
+		return map(Either::right, Either::left);
+	}
+	
+	private static final class Left<L, R> extends Either<L, R> {
+		
+		private final L value;
+		
+		public Left(L value) {
+			this.value = value;
+		}
+		
+		@Override
+		public <C, D> Either<C, D> mapBoth(Function<? super L, ? extends C> leftFunction, Function<? super R, ? extends D> rightFunction) {
+			return new Left<>(leftFunction.apply(this.value));
+		}
+		
+		@Override
+		public <T> T map(Function<? super L, ? extends T> leftFunction, Function<? super R, ? extends T> rightFunction) {
+			return leftFunction.apply(this.value);
+		}
+		
+		@Override
+		public boolean isLeft() {
+			return true;
+		}
+		
+		@Override
+		public boolean isRight() {
+			return false;
+		}
+		
+		@Override
+		public Either<L, R> ifLeft(Consumer<? super L> consumer) {
+			consumer.accept(this.value);
+			return this;
+		}
+		
+		@Override
+		public Either<L, R> ifRight(Consumer<? super R> consumer) {
+			return this;
+		}
+		
+		@Override
+		public Optional<L> left() {
+			return Optional.of(this.value);
+		}
+		
+		@Override
+		public Optional<R> right() {
+			return Optional.empty();
+		}
+		
+		@Override
+		public boolean equals(Object object) {
+			if (this == object) {
+				return true;
+			}
+			if (!(object instanceof Left)) {
+				return false;
+			}
+			Left<?, ?> other = (Left<?, ?>) object;
+			return Objects.equals(this.value, other.value);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(this.value);
+		}
+	}
+	
+	private static final class Right<L, R> extends Either<L, R> {
+		
+		private final R value;
+		
+		public Right(R value) {
+			this.value = value;
+		}
+		
+		@Override
+		public <C, D> Either<C, D> mapBoth(Function<? super L, ? extends C> leftFunction, Function<? super R, ? extends D> rightFunction) {
+			return new Right<>(rightFunction.apply(this.value));
+		}
+		
+		@Override
+		public <T> T map(Function<? super L, ? extends T> leftFunction, Function<? super R, ? extends T> rightFunction) {
+			return rightFunction.apply(this.value);
+		}
+		
+		@Override
+		public boolean isLeft() {
+			return false;
+		}
+		
+		@Override
+		public boolean isRight() {
+			return true;
+		}
+		
+		@Override
+		public Either<L, R> ifLeft(Consumer<? super L> consumer) {
+			return this;
+		}
+		
+		@Override
+		public Either<L, R> ifRight(Consumer<? super R> consumer) {
+			consumer.accept(this.value);
+			return this;
+		}
+		
+		@Override
+		public Optional<L> left() {
+			return Optional.empty();
+		}
+		
+		@Override
+		public Optional<R> right() {
+			return Optional.of(this.value);
+		}
+		
+		@Override
+		public boolean equals(Object object) {
+			if (this == object) {
+				return true;
+			}
+			if (!(object instanceof Right)) {
+				return false;
+			}
+			Right<?, ?> other = (Right<?, ?>) object;
+			return Objects.equals(this.value, other.value);
+		}
+		
+		@Override
+		public int hashCode() {
+			return Objects.hash(this.value);
+		}
+		
+	}
+	
+}
