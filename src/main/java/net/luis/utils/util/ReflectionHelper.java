3dfa2e308fe525bf700cecf4cc747c9bb2838257
Luis <76595940+Luis-st@users.noreply.github.com>
2022-12-22 19:46:43 +0100

    removed Builder and Creator

--- src/main/java/net/luis/utils/util/ReflectionHelper.java
+++ src/main/java/net/luis/utils/util/ReflectionHelper.java
@@ -23,6 +23,7 @@ public class ReflectionHelper {
 	
 	private static final Logger LOGGER = LogManager.getLogger();
 	private static boolean LOG_EXCEPTIONS = false;
+	private static boolean THROW_EXCEPTIONS = false;
 	
 	public static void enableExceptionLogging() {
 		LOG_EXCEPTIONS = true;
@@ -32,8 +33,12 @@ public class ReflectionHelper {
 		LOG_EXCEPTIONS = false;
 	}
 	
-	public static boolean shouldLogExceptions() {
-		return LOG_EXCEPTIONS;
+	public static void enableExceptionThrowing() {
+		THROW_EXCEPTIONS = true;
+	}
+	
+	public static void disableExceptionThrowing() {
+		THROW_EXCEPTIONS = false;
 	}
 	
 	@Nullable
@@ -42,8 +47,10 @@ public class ReflectionHelper {
 			return Class.forName(className);
 		} catch (ClassNotFoundException e) {
 			LOGGER.warn("The type {} could not be found", className);
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		}
 		return null;
@@ -70,14 +77,18 @@ public class ReflectionHelper {
 		try {
 			constructor = clazz.getDeclaredConstructor(parameters);
 		} catch (NoSuchMethodException e) {
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
+				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
 				throw new RuntimeException(e);
 			}
 			LOGGER.warn("Error retrieving the constructor for parameter {} in type {}", getSimpleNames(parameters), clazz.getSimpleName());
 		} catch (SecurityException e) {
 			LOGGER.warn("No permission to retrieve constructor of type {}", clazz.getSimpleName());
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		}
 		return constructor;
@@ -104,23 +115,31 @@ public class ReflectionHelper {
 			}
 		} catch (InstantiationException e) {
 			LOGGER.warn("Cannot create a new instance of type {} with arguments {}", constructor.getDeclaringClass().getSimpleName(), getSimpleNames(parameters));
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		} catch (IllegalAccessException e) {
 			LOGGER.warn("Access to the constructor of type {} not possible", constructor.getDeclaringClass().getSimpleName());
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		} catch (IllegalArgumentException e) {
 			LOGGER.warn("The parameters {} do not match those of the constructor in type {}, expected parameters {}", getSimpleNames(constructor.getParameterTypes()), constructor.getClass().getSimpleName());
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		} catch (InvocationTargetException e) {
 			LOGGER.warn("Something went wrong when invoking constructor of type {} with parameters {}", constructor.getDeclaringClass().getSimpleName(), getSimpleNames(parameters));
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		}
 		return instance;
@@ -138,13 +157,17 @@ public class ReflectionHelper {
 			method = clazz.getDeclaredMethod(name, parameters);
 		} catch (NoSuchMethodException e) {
 			LOGGER.warn("Error retrieving method for name {} and parameter {} in type {}", name, getSimpleNames(parameters), clazz.getSimpleName());
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		} catch (SecurityException e) {
 			LOGGER.warn("No permission to retrieve method with name {} and parameters {} in type {}", name, getSimpleNames(parameters), clazz.getSimpleName());
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		}
 		return method;
@@ -171,18 +194,24 @@ public class ReflectionHelper {
 			}
 		} catch (IllegalAccessException e) {
 			LOGGER.warn("Access to the method {} of type {} and parameter {} not possible", method.getName(), method.getDeclaringClass().getSimpleName(), getSimpleNames(parameters));
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		} catch (IllegalArgumentException e) {
 			LOGGER.warn("The parameters {} do not match those of the method {} in type {}, expected parameters {}", getSimpleNames(parameters), method.getName(), method.getDeclaringClass().getSimpleName(), getSimpleNames(method.getParameterTypes()));
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		} catch (InvocationTargetException e) {
 			LOGGER.warn("Something went wrong when invoking method {} in class {} and parameters {}", method.getName(), method.getDeclaringClass().getSimpleName(), getSimpleNames(parameters));
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		}
 		return returnValue;
@@ -200,13 +229,17 @@ public class ReflectionHelper {
 			field = clazz.getDeclaredField(name);
 		} catch (NoSuchFieldException e) {
 			LOGGER.warn("Fail to get field {} in class {}", name, clazz.getSimpleName());
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		} catch (SecurityException e) {
 			LOGGER.warn("No permisson to get field {} in class {}", name, clazz.getSimpleName());
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		}
 		return field;
@@ -233,13 +266,17 @@ public class ReflectionHelper {
 			}
 		} catch (IllegalArgumentException e) {
 			LOGGER.warn("Value of the field {} in class {} cannot be determined", field.getName(), instance.getClass().getSimpleName());
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		} catch (IllegalAccessException e) {
 			LOGGER.warn("Access to field {} of type {} not possible", field.getName(), field.getDeclaringClass().getSimpleName());
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		}
 		return value;
@@ -259,13 +296,17 @@ public class ReflectionHelper {
 			}
 		} catch (IllegalArgumentException e) {
 			LOGGER.warn("Value of the field with the name {} in type {} could not be set", field.getName(), instance.getClass().getSimpleName());
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		} catch (IllegalAccessException e) {
 			LOGGER.warn("Can not access field {} in class {}", field.getName(), field.getDeclaringClass().getSimpleName());
-			if (LOG_EXCEPTIONS) {
+			if (LOG_EXCEPTIONS && !THROW_EXCEPTIONS) {
 				LOGGER.error(e);
+			} else if (THROW_EXCEPTIONS) {
+				throw new RuntimeException(e);
 			}
 		}
 	}
