45d6ed4812ca6bef156d5bf54b7541bdbf1d0cd8
Luis <luis@staudt-fridingen.de>
2023-03-10 19:23:22 +0100

    removed ByteTags and ShortTags

--- src/test/java/net/luis/utils/data/tag/tags/CompoundTagTest.java
+++ src/test/java/net/luis/utils/data/tag/tags/CompoundTagTest.java
@@ -22,13 +22,10 @@ class CompoundTagTest {
 		Path path = new File("D:\\Programmieren\\Git Repositories\\LUtils\\build\\tests\\compound_tag_test.txt").toPath();
 		CompoundTag tagSave = new CompoundTag();
 		tagSave.putBoolean("Boolean", true);
-		tagSave.putByte("Byte", (byte) 12);
-		tagSave.putShort("Short", (short) 13);
 		tagSave.putInt("Int", 14);
 		tagSave.putLong("Long", 15L);
 		tagSave.putFloat("Float", 16.0F);
 		tagSave.putDouble("Double", 17.0);
-		tagSave.putByteArray("ByteArray", new byte[] {0, 1, 2, 3});
 		tagSave.putIntArray("IntArray", new int[] {4, 5, 6, 7});
 		tagSave.putLongArray("LongArray", new long[] {8, 9, 10, 11});
 		tagSave.putString("String", "String");
@@ -38,15 +35,12 @@ class CompoundTagTest {
 		Tag tag = Tag.load(path);
 		assertInstanceOf(CompoundTag.class, tag);
 		if (tag instanceof CompoundTag tagLoad) {
-			assertEquals(tagLoad.size(), 13);
+			assertEquals(tagLoad.size(), 10);
 			assertTrue(tagLoad.getBoolean("Boolean"));
-			assertEquals(tagLoad.getByte("Byte"), (byte) 12);
-			assertEquals(tagLoad.getShort("Short"), (short) 13);
 			assertEquals(tagLoad.getInt("Int"), 14);
 			assertEquals(tagLoad.getLong("Long"), 15L);
 			assertEquals(tagLoad.getFloat("Float"), 16.0F);
 			assertEquals(tagLoad.getDouble("Double"), 17.0);
-			assertArrayEquals(tagLoad.getByteArray("ByteArray"), new byte[] {0, 1, 2, 3});
 			assertArrayEquals(tagLoad.getIntArray("IntArray"), new int[] {4, 5, 6, 7});
 			assertArrayEquals(tagLoad.getLongArray("LongArray"), new long[] {8, 9, 10, 11});
 			assertEquals(tagLoad.getString("String"), "String");
