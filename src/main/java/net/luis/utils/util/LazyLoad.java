c73d809d2b0ae6907e1731257dbbfd6d902332d9
Luis <luis@staudt-fridingen.de>
2023-03-10 19:22:58 +0100

    added LazyLoad

---
+++ src/main/java/net/luis/utils/util/LazyLoad.java
@@ -0,0 +1,54 @@
+package net.luis.utils.util;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ *
+ * @author Luis-st
+ *
+ */
+
+public class LazyLoad<T> {
+	
+	private final Supplier<T> supplier;
+	private T value;
+	
+	public LazyLoad(Supplier<T> supplier) {
+		this.supplier = supplier;
+	}
+	
+	public void load() {
+		if (this.value == null) {
+			this.value = this.supplier.get();
+		}
+	}
+	
+	public boolean isLoaded() {
+		return this.value != null;
+	}
+	
+	public T get() {
+		this.load();
+		return this.value;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof LazyLoad<?> lazyLoad)) return false;
+		
+		return Objects.equals(this.value, lazyLoad.value);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.value);
+	}
+	
+	@Override
+	public String toString() {
+		return ToString.toString(this, "supplier");
+	}
+	
+}
---
+++ src/test/java/net/luis/utils/util/LazyLoadTest.java
@@ -0,0 +1,27 @@
+package net.luis.utils.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Luis-st
+ *
+ */
+
+class LazyLoadTest {
+	
+	@Test
+	void isLoaded() {
+		LazyLoad<String> lazyLoad = new LazyLoad<>(() -> "Hello World!");
+		assertFalse(lazyLoad.isLoaded());
+		lazyLoad.load();
+		assertTrue(lazyLoad.isLoaded());
+	}
+	
+	@Test
+	void get() {
+		assertEquals("Hello World!", new LazyLoad<>(() -> "Hello World!").get());
+	}
+}
