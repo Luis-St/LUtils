b634feb320ae5a664aeaf4e8f23e00777156802e
Luis <luis@staudt-fridingen.de>
2023-03-24 18:55:53 +0100

    added Nullability

--- src/main/java/net/luis/utils/util/reflection/ReflectionUtils.java
+++ src/main/java/net/luis/utils/util/reflection/ReflectionUtils.java
@@ -4,8 +4,11 @@ import com.google.common.collect.Lists;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.Serializable;
+import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -17,74 +20,30 @@ import java.util.Map;
 
 public class ReflectionUtils {
 	
-	public static void invokeMethod(Method method, Object instance, Object... args) {
-		if (hasDuplicates(args)) {
-			throw new IllegalArgumentException("The arguments must not contain duplicates");
+	public static @NotNull Nullability getNullability(@NotNull AnnotatedType type) {
+		if (type.isAnnotationPresent(Nullable.class)) {
+			return Nullability.NULLABLE;
+		} else if (type.isAnnotationPresent(NotNull.class)) {
+			return Nullability.NOT_NULL;
 		}
-		List<ParameterInfo> parameterInfos = Lists.newArrayList();
-		for (Object arg : args) {
-			parameterInfos.add(new ParameterInfo(arg, false, "", arg == null));
-		}
-		invokeMethod(method, instance, parameterInfos);
+		return Nullability.UNKNOWN;
 	}
 	
-	private static boolean hasDuplicates(Object... args) {
-	    List<Class<?>> classes = Lists.newArrayList();
-		for (Object arg : args) {
-			if (classes.contains(arg.getClass())) {
-				return true;
-			}
-			classes.add(arg.getClass());
+	public static @NotNull String getRawName(@NotNull Method method, String... prefixes) {
+		String name = method.getName();
+		if (name.startsWith("get")) {
+			return name.substring(3);
+		} else if (name.startsWith("is")) {
+			return name.substring(2);
+		} else if (name.startsWith("has")) {
+			return name.substring(3);
 		}
-		return false;
-	}
-	
-	public static void invokeMethod(Method method, Object instance, List<ParameterInfo> parameterInfos) {
-		List<Object> parameters = Lists.newArrayList();
-		for (Parameter parameter : method.getParameters()) {
-			parameters.add(getParameter(parameter, parameterInfos));
-		}
-		if (parameters.size() != method.getParameters().length) {
-			throw new IllegalArgumentException("The given parameters do not match the parameters of the method");
-		}
-		ReflectionHelper.invoke(method, instance, parameters.toArray());
-	}
-	
-	private static ParameterInfo getParameter(Parameter parameter, List<ParameterInfo> parameterInfos) {
-		for (ParameterInfo info : parameterInfos) {
-			boolean duplicates = hasDuplicates(parameterInfos);
-			if (info.type() == parameter.getType()) {
-				if (!duplicates) {
-					return checkNullability(info, parameter);
-				} else if (info.hasName() && info.parameterName().equals(parameter.getName())) {
-					return checkNullability(info, parameter);
-				}
+		for (String prefix : prefixes) {
+			if (name.startsWith(prefix)) {
+				return name.substring(prefix.length());
 			}
 		}
-		for (ParameterInfo info : parameterInfos) {
-			if (info.type() == parameter.getType()) {
-				return info;
-			}
-		}
-		throw new IllegalArgumentException("Could not find a parameter with the type " + parameter.getType().getName() + " in the given parameters");
-	}
-	
-	private static ParameterInfo checkNullability(ParameterInfo info, Parameter parameter) {
-		if (info.nullable() && !parameter.isAnnotationPresent(Nullable.class)) {
-			throw new IllegalArgumentException("The parameter " + parameter.getName() + " of the method " + parameter.getDeclaringExecutable().getName() + " is not marked as nullable");
-		}
-		return info;
-	}
-	
-	private static boolean hasDuplicates(List<ParameterInfo> parameterInfos) {
-		List<Class<?>> classes = Lists.newArrayList();
-		for (ParameterInfo info : parameterInfos) {
-			if (classes.contains(info.type())) {
-				return true;
-			}
-			classes.add(info.type());
-		}
-		return false;
+		return name;
 	}
 	
 }
