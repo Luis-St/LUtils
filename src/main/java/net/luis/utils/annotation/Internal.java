5bbecfe52f096876213985268417869c2a4f2e46
Luis <luis@staudt-fridingen.de>
2023-05-24 12:40:37 +0200

    added Internal annotation

---
+++ src/main/java/net/luis/utils/annotation/Internal.java
@@ -0,0 +1,16 @@
+package net.luis.utils.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ *
+ * @author Luis
+ *
+ */
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Internal {
+
+}
