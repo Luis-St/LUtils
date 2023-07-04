94a5e945f9a4d12d9c7df5744a0324dce54718ba
Luis <76595940+Luis-st@users.noreply.github.com>
2023-07-04 10:38:42 +0200

    added FileUtils

---
+++ src/test/java/net/luis/utils/io/FileUtilsTest.java
@@ -0,0 +1,58 @@
+package net.luis.utils.io;
+
+import net.luis.utils.util.Pair;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileUtilsTest {
+	
+	@Test
+	void split() {
+		assertDoesNotThrow(() -> FileUtils.split(null));
+		assertDoesNotThrow(() -> FileUtils.split(""));
+		assertEquals(Pair.of("", ""), FileUtils.split(""));
+		assertEquals(Pair.of("/", ""), FileUtils.split("/"));
+		assertEquals(Pair.of("/", "test"), FileUtils.split("/test"));
+		assertEquals(Pair.of("/test/", ""), FileUtils.split("/test/"));
+		assertEquals(Pair.of("/", "test.json"), FileUtils.split("/test.json"));
+		assertEquals(Pair.of("/test/", "test.json"), FileUtils.split("/test/test.json"));
+	}
+	
+	@Test
+	void getName() {
+		assertDoesNotThrow(() -> FileUtils.getName(null));
+		assertDoesNotThrow(() -> FileUtils.getName(""));
+		assertEquals("", FileUtils.getName(""));
+		assertEquals("", FileUtils.getName("/"));
+		assertEquals("test", FileUtils.getName("/test"));
+		assertEquals("test", FileUtils.getName("/test.json"));
+		assertEquals("test", FileUtils.getName("/test/test.json"));
+	}
+	
+	@Test
+	void getExtension() {
+		assertDoesNotThrow(() -> FileUtils.getExtension(null));
+		assertDoesNotThrow(() -> FileUtils.getExtension(""));
+		assertEquals("", FileUtils.getExtension(""));
+		assertEquals("", FileUtils.getExtension("/"));
+		assertEquals("", FileUtils.getExtension("/test"));
+		assertEquals("json", FileUtils.getExtension("/test.json"));
+		assertEquals("json", FileUtils.getExtension("/test/test.json"));
+	}
+	
+	@Test
+	void getRelativePath() {
+		assertDoesNotThrow(() -> FileUtils.getRelativePath(null));
+		assertDoesNotThrow(() -> FileUtils.getRelativePath(""));
+		assertEquals("./", FileUtils.getRelativePath(null));
+		assertEquals("./", FileUtils.getRelativePath(""));
+		assertEquals("./", FileUtils.getRelativePath("/"));
+		assertEquals("./test/", FileUtils.getRelativePath("/test"));
+		assertEquals("./test/", FileUtils.getRelativePath("/test/"));
+		assertEquals("./test/", FileUtils.getRelativePath("./test/"));
+		assertEquals("./test/", FileUtils.getRelativePath("test/test.json"));
+		assertEquals("./test/", FileUtils.getRelativePath("/test/test.json"));
+		assertEquals("./test/", FileUtils.getRelativePath("./test/test.json"));
+	}
+}
