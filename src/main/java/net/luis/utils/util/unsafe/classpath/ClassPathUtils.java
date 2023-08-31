fa8d8ea76e5f290331aab577b6c15901dcf348bb
Luis <76595940+Luis-st@users.noreply.github.com>
2023-08-31 14:44:53 +0200

    added helper method to get single annotated field

--- src/main/java/net/luis/utils/util/unsafe/classpath/ClassPathUtils.java
+++ src/main/java/net/luis/utils/util/unsafe/classpath/ClassPathUtils.java
@@ -1,6 +1,7 @@
 package net.luis.utils.util.unsafe.classpath;
 
 import com.google.common.collect.Lists;
+import net.luis.utils.util.unsafe.reflection.ReflectionHelper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -8,8 +9,7 @@ import org.jetbrains.annotations.NotNull;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -18,6 +18,7 @@ import java.util.stream.Collectors;
  *
  */
 
+@SuppressWarnings("ErrorNotRethrown")
 public class ClassPathUtils {
 	
 	private static final Logger LOGGER = LogManager.getLogger(ClassPathUtils.class);
@@ -100,11 +101,21 @@ public class ClassPathUtils {
 		Objects.requireNonNull(annotation, "Annotation must not be null");
 		return Lists.newArrayList(clazz.getDeclaredFields()).stream().filter(field -> field.isAnnotationPresent(annotation)).collect(Collectors.toList());
 	}
+	
+	public static @NotNull Optional<Field> getAnnotatedField(@NotNull Class<?> clazz, String name, Class<? extends Annotation> annotation) {
+		Objects.requireNonNull(clazz, "Class must not be null");
+		Objects.requireNonNull(name, "Name must not be null");
+		Objects.requireNonNull(annotation, "Annotation must not be null");
+		if (ReflectionHelper.hasField(clazz, name, f -> f.isAnnotationPresent(annotation))) {
+			return Optional.of(ReflectionHelper.getField(clazz, name));
+		}
+		return Optional.empty();
+	}
 	//endregion
 	
 	//region Internal
-	public static enum Mode {
-		INCLUDE, EXCLUDE;
+	public enum Mode {
+		INCLUDE, EXCLUDE
 	}
 	//endregion
 }
