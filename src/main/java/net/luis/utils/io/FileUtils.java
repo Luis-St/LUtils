7a5256b899f708bc8c27c5c7fbfd8bc12604b3ac
Luis <luis@staudt-fridingen.de>
2023-12-29 22:50:41 +0100

    added file and directory io helpers

--- src/main/java/net/luis/utils/io/FileUtils.java
+++ src/main/java/net/luis/utils/io/FileUtils.java
@@ -5,6 +5,12 @@ import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.*;
+import java.nio.file.*;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 /**
  *
  * @author Luis-St
@@ -15,13 +21,16 @@ public class FileUtils {
 	
 	public static @NotNull Pair<String, String> split(@Nullable String file) {
 		String str = StringUtils.stripToEmpty(file).replace("\\", "/");
-		int index = str.lastIndexOf("/");
-		if (index == -1) {
+		int dash = str.lastIndexOf("/");
+		int dot = str.lastIndexOf(".");
+		if (dash == -1 && dot == -1) {
+			return Pair.of(str, "");
+		} else if (dash == -1) {
 			return Pair.of("", str);
-		} else if (getExtension(file).isEmpty()) {
+		} else if (dot == -1) {
 			return Pair.of(str, "");
 		} else {
-			return Pair.of(str.substring(0, index + 1), str.substring(index + 1));
+			return Pair.of(str.substring(0, dash + 1), str.substring(dash + 1));
 		}
 	}
 	
@@ -64,5 +73,50 @@ public class FileUtils {
 			return "." + str;
 		}
 		return str;
+	}
+	
+	public static void create(@NotNull Path file) throws IOException {
+		Objects.requireNonNull(file, "File must not be null");
+		if (Files.exists(file)) {
+			throw new FileAlreadyExistsException("File '" + file + "' already exists");
+		}
+		Pair<String, String> pair = split(file.toString());
+		String folders = pair.getFirst();
+		String name = pair.getSecond();
+		if (!".".equals(folders) && !"./".equals(folders)) {
+			Files.createDirectories(Paths.get(folders));
+		}
+		if (!name.isBlank()) {
+			Files.createFile(file);
+		}
+	}
+	
+	public static void createIfNotExists(@NotNull Path file) throws IOException {
+		Objects.requireNonNull(file, "File must not be null");
+		if (Files.notExists(file)) {
+			create(file);
+		}
+	}
+	
+	public static @NotNull Path createSessionDirectory(@Nullable String prefix) throws IOException {
+		Path path = Files.createTempDirectory(prefix == null ? "temp" : prefix);
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			try {
+				deleteRecursively(path.toFile().toPath());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}));
+		return path;
+	}
+	
+	public static void deleteRecursively(@NotNull Path path) throws IOException {
+		Objects.requireNonNull(path, "Path must not be null");
+		if (!Files.exists(path)) {
+			throw new FileNotFoundException("Path '" + path + "' does not exist");
+		}
+		try (Stream<Path> files = Files.walk(path)) {
+			files.sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+		}
 	}
 }
--- src/test/java/net/luis/utils/io/FileUtilsTest.java
+++ src/test/java/net/luis/utils/io/FileUtilsTest.java
@@ -1,18 +1,36 @@
 package net.luis.utils.io;
 
 import net.luis.utils.util.Pair;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileUtilsTest {
+	
+	//region Clean up
+	@AfterAll
+	static void tearDownAfter() throws Exception {
+		Files.deleteIfExists(Path.of("test.json"));
+		Files.deleteIfExists(Path.of("test/test.json"));
+		Files.deleteIfExists(Path.of("test"));
+		Files.deleteIfExists(Path.of("test.xml"));
+	}
+	//endregion
 	
 	@Test
 	void split() {
 		assertDoesNotThrow(() -> FileUtils.split(null));
 		assertDoesNotThrow(() -> FileUtils.split(""));
 		assertEquals(Pair.of("", ""), FileUtils.split(""));
+		assertEquals(Pair.of("", "."), FileUtils.split("."));
 		assertEquals(Pair.of("/", ""), FileUtils.split("/"));
+		assertEquals(Pair.of("./", ""), FileUtils.split("./"));
+		assertEquals(Pair.of("test", ""), FileUtils.split("test"));
 		assertEquals(Pair.of("/test", ""), FileUtils.split("/test"));
 		assertEquals(Pair.of("/test/", ""), FileUtils.split("/test/"));
 		assertEquals(Pair.of("/", "test.json"), FileUtils.split("/test.json"));
@@ -54,5 +72,49 @@ class FileUtilsTest {
 		assertEquals("./test/", FileUtils.getRelativePath("test/test.json"));
 		assertEquals("./test/", FileUtils.getRelativePath("/test/test.json"));
 		assertEquals("./test/", FileUtils.getRelativePath("./test/test.json"));
+	}
+	
+	@Test
+	void create() throws Exception {
+		Path file = Path.of("test.json");
+		Path directory = Path.of("test");
+		Path both = Path.of("test/test.json");
+		
+		assertDoesNotThrow(() -> FileUtils.create(file));
+		assertTrue(Files.exists(file));
+		assertDoesNotThrow(() -> FileUtils.create(directory));
+		assertTrue(Files.exists(directory));
+		assertDoesNotThrow(() -> FileUtils.create(both));
+		assertTrue(Files.exists(both));
+		assertThrows(NullPointerException.class, () -> FileUtils.create(null));
+		assertThrows(IOException.class, () -> FileUtils.create(file));
+	}
+	
+	@Test
+	void createIfNotExists() {
+		Path file = Path.of("test.xml");
+		assertFalse(Files.exists(file));
+		assertDoesNotThrow(() -> FileUtils.createIfNotExists(file));
+		assertTrue(Files.exists(file));
+		assertDoesNotThrow(() -> FileUtils.createIfNotExists(file));
+		assertThrows(NullPointerException.class, () -> FileUtils.createIfNotExists(null));
+	}
+	
+	@Test
+	void createSessionDirectory() {
+		assertDoesNotThrow(() -> FileUtils.createSessionDirectory(null));
+		assertDoesNotThrow(() -> FileUtils.createSessionDirectory(""));
+		assertDoesNotThrow(() -> FileUtils.createSessionDirectory("test"));
+	}
+	
+	@Test
+	void deleteRecursively() throws IOException {
+		Path file = Path.of("temp/temp/test.json");
+		assertDoesNotThrow(() -> FileUtils.create(file));
+		assertTrue(Files.exists(file));
+		assertDoesNotThrow(() -> FileUtils.deleteRecursively(Path.of("temp")));
+		assertFalse(Files.exists(Path.of("temp")));
+		assertThrows(NullPointerException.class, () -> FileUtils.deleteRecursively(null));
+		assertThrows(IOException.class, () -> FileUtils.deleteRecursively(file));
 	}
 }
