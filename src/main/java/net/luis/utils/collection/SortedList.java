86e8304c0c3c5e8a0442b3fb05cd4846087f901b
Luis <luis@staudt-fridingen.de>
2024-01-14 21:31:21 +0100

    added javadocs to SortedList

--- src/main/java/net/luis/utils/collection/SortedList.java
+++ src/main/java/net/luis/utils/collection/SortedList.java
@@ -8,48 +8,96 @@ import org.jetbrains.annotations.Nullable;
 import java.util.*;
 
 /**
+ * A list which is after every modification sorted<br>
+ * using the set comparator or if it is {@code null} the natural order.<br>
  *
  * @author Luis-St
- *
  */
-
 public class SortedList<E> extends AbstractList<E> {
 	
+	/**
+	 * The internal list.
+	 */
 	private final List<E> internalList;
+	/**
+	 * The comparator used to sort the list.
+	 */
 	private Comparator<E> comparator;
 	
+	/**
+	 * Constructs a new empty sorted list.<br>
+	 * The comparator is set to {@code null}.<br>
+	 * Internally, a {@link ArrayList} is used.<br>
+	 */
 	public SortedList() {
 		this(Lists.newArrayList());
 	}
 	
+	/**
+	 * Constructs a new empty sorted list with the given comparator.<br>
+	 * Internally, a {@link ArrayList} is used.<br>
+	 * @param comparator The comparator used to sort the list
+	 */
 	public SortedList(@Nullable Comparator<E> comparator) {
 		this(Lists.newArrayList(), comparator);
 	}
 	
+	/**
+	 * Constructs a new sorted list with the given elements.<br>
+	 * The comparator is set to {@code null}.<br>
+	 * The elements are wrapped in an internal {@link ArrayList}.<br>
+	 * @param elements The elements to add
+	 */
 	@SafeVarargs
 	public SortedList(E @NotNull ... elements) {
 		this(Lists.newArrayList(elements));
 	}
 	
+	/**
+	 * Constructs a new sorted list with the given elements and the comparator.<br>
+	 * The elements are wrapped in an internal {@link ArrayList}.<br>
+	 * @param comparator The comparator used to sort the list
+	 * @param elements The elements to add
+	 */
 	@SafeVarargs
 	public SortedList(@Nullable Comparator<E> comparator, E @NotNull ... elements) {
 		this(Lists.newArrayList(elements), comparator);
 	}
 	
+	/**
+	 * Constructs a new sorted list with from the given list.<br>
+	 * The comparator is set to {@code null}.<br>
+	 * The list is not copied, it is used directly.<br>
+	 * @param elements The list to use
+	 */
 	public SortedList(@NotNull List<E> elements) {
 		this(elements, null);
 	}
 	
+	/**
+	 * Constructs a new sorted list from the given list and the comparator.<br>
+	 * The list is not copied, it is used directly.<br>
+	 * @param elements The list to use
+	 * @param comparator The comparator used to sort the list
+	 */
 	public SortedList(@NotNull List<E> elements, @Nullable Comparator<E> comparator) {
 		this.internalList = elements;
 		this.comparator = comparator;
 	}
 	
+	/**
+	 * Sets the comparator used to sort the list.<br>
+	 * @param comparator The comparator to set
+	 */
 	public void setComparator(@Nullable Comparator<E> comparator) {
 		this.comparator = comparator;
 		this.internalList.sort(comparator);
 	}
 	
+	/**
+	 * {@inheritDoc}<br>
+	 * The list is sorted after the element is added.
+	 */
 	@Override
 	public E set(int index, @NotNull E element) {
 		Objects.requireNonNull(element, "Element must not be null");
@@ -58,6 +106,10 @@ public class SortedList<E> extends AbstractList<E> {
 		return old;
 	}
 	
+	/**
+	 * {@inheritDoc}<br>
+	 * The list is sorted after the element is added.
+	 */
 	@Override
 	public void add(int index, @NotNull E element) {
 		Objects.requireNonNull(element, "Element must not be null");
@@ -65,6 +117,10 @@ public class SortedList<E> extends AbstractList<E> {
 		this.internalList.sort(this.comparator);
 	}
 	
+	/**
+	 * {@inheritDoc}<br>
+	 * The list is sorted after the element is removed.
+	 */
 	@Override
 	public boolean remove(@Nullable Object object) {
 		boolean removed = this.internalList.remove(object);
@@ -82,6 +138,10 @@ public class SortedList<E> extends AbstractList<E> {
 		return this.internalList.size();
 	}
 	
+	/**
+	 * The list is sorted with the comparator of this list.<br>
+	 * @param comparator The ignored comparator
+	 */
 	@Override
 	public void sort(@Ignored.Always @Nullable Comparator<? super E> comparator) {
 		super.sort(this.comparator);
