123c78d34de8a43a89ce8f6b253122a8c4795660
Luis <luis@staudt-fridingen.de>
2024-02-02 18:40:04 +0100

    added javadocs to StackTraceUtils

--- src/main/java/net/luis/utils/util/unsafe/StackTraceUtils.java
+++ src/main/java/net/luis/utils/util/unsafe/StackTraceUtils.java
@@ -9,23 +9,73 @@ import java.lang.reflect.Method;
 import java.util.*;
 
 /**
+ * Utility class for stack trace related operations.<br>
+ * Provides methods to get the calling class and method.<br>
  *
  * @author Luis-St
- *
  */
-
 public class StackTraceUtils {
 	
+	/**
+	 * Constant for the system property 'unsafe.calls.main'.<br>
+	 * If this property is {@code true}, the methods in this class can be called from the {@code main} method.<br>
+	 */
 	private static final String UNSAFE_CALLS_MAIN = "unsafe.calls.main";
 	
+	/**
+	 * Gets the calling class from the stack trace with an offset of {@code 4}.<br>
+	 * <ul>
+	 *     <li>...</li>
+	 *     <li>2: {@link StackTraceUtils#getCallingClass()}</li>
+	 *     <li>3: The method which requests the calling class</li>
+	 *     <li>4: The calling class</li>
+	 * </ul>
+	 * @return The calling class
+	 * @throws IndexOutOfBoundsException If the specified position is out of bounds
+	 * @throws IllegalCallerException If called from the {@code main} method and 'unsafe.calls.main' is {@code false}
+	 * @see StackTraceUtils#getCallingClass(int)
+	 * @see StackTraceUtils#getStackTrace(int)
+	 */
 	public static Class<?> getCallingClass() {
 		return ReflectionHelper.getClassForName(getStackTrace(1).getClassName());
 	}
 	
+	/**
+	 * Gets the calling class from the stack trace with an offset of {@code 4 + callsBefore}.<br>
+	 * <ul>
+	 *     <li>...</li>
+	 *     <li>2: {@link StackTraceUtils#getCallingClass(int)}</li>
+	 *     <li>3: The class with the method which calls this method</li>
+	 *     <li>4: The calling class</li>
+	 *     <li>...</li>
+	 * </ul>
+	 * @param callsBefore The number of calls before the method which requests the calling class
+	 * @return The calling class at the specified position
+	 * @throws IllegalArgumentException If the specified calls before value is less than or equal to 0
+	 * @throws IndexOutOfBoundsException If the specified position is out of bounds
+	 * @throws IllegalCallerException If called from the {@code main} method and 'unsafe.calls.main' is {@code false}
+	 * @see StackTraceUtils#getCallingClass()
+	 * @see StackTraceUtils#getStackTrace(int)
+	 */
 	public static Class<?> getCallingClass(int callsBefore) {
 		return ReflectionHelper.getClassForName(getStackTrace(1 + callsBefore).getClassName());
 	}
 	
+	/**
+	 * Gets the calling method from the stack trace with an offset of {@code 4}.<br>
+	 * <ul>
+	 *     <li>...</li>
+	 *     <li>2: {@link StackTraceUtils#getCallingMethod()}</li>
+	 *     <li>3: The method which requests the calling method</li>
+	 *     <li>4: The calling method</li>
+	 * </ul>
+	 * @return The calling method
+	 * @throws IndexOutOfBoundsException If the specified position is out of bounds
+	 * @throws IllegalStateException If the calling method could not be identified
+	 * @throws IllegalCallerException If called from the {@code main} method and 'unsafe.calls.main' is {@code false}
+	 * @see StackTraceUtils#getCallingMethod(int)
+	 * @see StackTraceUtils#getStackTrace(int)
+	 */
 	public static @NotNull Method getCallingMethod() {
 		StackTraceElement element = getStackTrace(1);
 		Method method = getCallingMethod(element);
@@ -36,6 +86,24 @@ public class StackTraceUtils {
 		}
 	}
 	
+	/**
+	 * Gets the calling method from the stack trace with an offset of {@code 4 + callsBefore}.<br>
+	 * <ul>
+	 *     <li>...</li>
+	 *     <li>2: {@link StackTraceUtils#getCallingMethod(int)}</li>
+	 *     <li>3: The method which calls this method</li>
+	 *     <li>4: The calling method</li>
+	 *     <li>...</li>
+	 * </ul>
+	 * @param callsBefore The number of calls before the method which requests the calling method
+	 * @return The calling method at the specified position
+	 * @throws IllegalArgumentException If the specified calls before value is less than or equal to 0
+	 * @throws IndexOutOfBoundsException If the specified position is out of bounds
+	 * @throws IllegalStateException If the calling method could not be identified
+	 * @throws IllegalCallerException If called from the {@code main} method and 'unsafe.calls.main' is {@code false}
+	 * @see StackTraceUtils#getCallingMethod()
+	 * @see StackTraceUtils#getStackTrace(int)
+	 */
 	public static @NotNull Method getCallingMethod(int callsBefore) {
 		StackTraceElement element = getStackTrace(1 + callsBefore);
 		Method method = getCallingMethod(element);
@@ -46,6 +114,21 @@ public class StackTraceUtils {
 		}
 	}
 	
+	/**
+	 * Gets the calling method safe from the stack trace with an offset of {@code 4}.<br>
+	 * <ul>
+	 *     <li>...</li>
+	 *     <li>2: {@link StackTraceUtils#getCallingMethodSafe()}</li>
+	 *     <li>3: The method which requests the calling method</li>
+	 *     <li>4: The calling method</li>
+	 *     <li>...</li>
+	 * </ul>
+	 * @return An optional containing the calling method or an empty optional if the calling method could not be identified or an error occurred
+	 * @throws IndexOutOfBoundsException If the specified position is out of bounds
+	 * @throws IllegalCallerException If called from the {@code main} method and 'unsafe.calls.main' is {@code false}
+	 * @see StackTraceUtils#getCallingMethodSafe(int)
+	 * @see StackTraceUtils#getStackTrace(int)
+	 */
 	public static @NotNull Optional<Method> getCallingMethodSafe() {
 		try {
 			StackTraceElement element = getStackTrace(1);
@@ -55,6 +138,24 @@ public class StackTraceUtils {
 		}
 	}
 	
+	/**
+	 * Gets the calling method safe from the stack trace with an offset of {@code 4 + callsBefore}.<br>
+	 * <ul>
+	 *     <li>...</li>
+	 *     <li>2: {@link StackTraceUtils#getCallingMethodSafe(int)}</li>
+	 *     <li>3: The method which calls this method</li>
+	 *     <li>4: The calling method</li>
+	 *     <li>5: The method which calls the method which requests the calling method</li>
+	 *     <li>...</li>
+	 * </ul>
+	 * @param callsBefore The number of calls before the method which requests the calling method
+	 * @return An optional containing the calling method or an empty optional if the calling method could not be identified or an error occurred
+	 * @throws IllegalArgumentException If the specified calls before value is less than or equal to 0
+	 * @throws IndexOutOfBoundsException If the specified position is out of bounds
+	 * @throws IllegalCallerException If called from the {@code main} method and 'unsafe.calls.main' is {@code false}
+	 * @see StackTraceUtils#getCallingMethodSafe()
+	 * @see StackTraceUtils#getStackTrace(int)
+	 */
 	public static @NotNull Optional<Method> getCallingMethodSafe(int callsBefore) {
 		try {
 			StackTraceElement element = getStackTrace(callsBefore);
@@ -65,6 +166,24 @@ public class StackTraceUtils {
 	}
 	
 	//region Internal
+	
+	/**
+	 * Gets the stack trace element at the specified position.<br>
+	 * This method is used internally to get the correct stack trace element.<br>
+	 * The base offset is {@code 3} and the default offset of any calling method is {@code 1}.<br>
+	 * <ul>
+	 *     <li>0: {@link Thread#getStackTrace()}</li>
+	 *     <li>1: {@link StackTraceUtils#getStackTrace(int)}</li>
+	 *     <li>2: The overloading method in this class</li>
+	 *     <li>3: The method which requests the calling stack trace element</li>
+	 *     <li>4: The calling method or class</li>
+	 * </ul>
+	 * @param callsBefore The number of calls before the overloading method in this class
+	 * @return The stack trace element at the specified position
+	 * @throws IllegalArgumentException If the specified calls before value is less than or equal to 0
+	 * @throws IndexOutOfBoundsException If the specified position is out of bounds
+	 * @throws IllegalCallerException If called from the {@code main} method and 'unsafe.calls.main' is {@code false}
+	 */
 	private static @NotNull StackTraceElement getStackTrace(int callsBefore) {
 		StackTraceElement[] elements = Thread.currentThread().getStackTrace();
 		if (0 >= callsBefore) {
@@ -80,6 +199,14 @@ public class StackTraceUtils {
 		return elements[3 + callsBefore];
 	}
 	
+	/**
+	 * Gets the calling method from the specified stack trace element.<br>
+	 * The calling method will be identified by the class name and the method name.<br>
+	 * If there are multiple methods with the same name in the same class, the method will not be identified.<br>
+	 * @param element The stack trace element
+	 * @return The calling method or {@code null} if the method could not be identified
+	 * @throws NullPointerException If the specified stack trace element is {@code null}
+	 */
 	private static @Nullable Method getCallingMethod(@NotNull StackTraceElement element) {
 		Objects.requireNonNull(element, "Stack trace element must not be null");
 		Class<?> clazz = Objects.requireNonNull(ReflectionHelper.getClassForName(element.getClassName()));
