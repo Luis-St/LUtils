40ac3063f775d7d17deab6db090b7916f5bd364e
Luis <luis@staudt-fridingen.de>
2024-02-02 21:59:13 +0100

    overhault exception handling in ReflectionHelper

--- src/main/java/net/luis/utils/util/unsafe/reflection/ReflectionHelper.java
+++ src/main/java/net/luis/utils/util/unsafe/reflection/ReflectionHelper.java
@@ -12,6 +12,8 @@ import java.util.Objects;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import static org.apache.commons.lang3.ArrayUtils.*;
+
 /**
  *
  * @author Luis-St
@@ -21,45 +23,22 @@ import java.util.stream.Collectors;
 public class ReflectionHelper {
 	
 	private static final Logger LOGGER = LogManager.getLogger(ReflectionHelper.class);
-	//region Debugging
-	private static boolean logExceptions = false;
-	private static boolean throwExceptions = false;
-	
-	public static void enableExceptionLogging() {
-		logExceptions = true;
-	}
-	
-	public static void disableExceptionLogging() {
-		logExceptions = false;
-	}
-	
-	public static void enableExceptionThrowing() {
-		throwExceptions = true;
-	}
-	
-	public static void disableExceptionThrowing() {
-		throwExceptions = false;
-	}
-	//endregion
+	private static final String REFLECTION_EXCEPTIONS_THROW = "reflection.exceptions.throw";
+	private static final String REFLECTION_EXCEPTIONS_LOG = "reflection.exceptions.log";
 	
 	public static @Nullable Class<?> getClassForName(@NotNull String className) {
+		Objects.requireNonNull(className, "Class name must not be null");
 		try {
 			return Class.forName(className);
 		} catch (ClassNotFoundException e) {
-			//region Exception handling
 			LOGGER.warn("The type {} could not be found", className);
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
-			//endregion
+			handleException(e);
 		}
 		return null;
 	}
 	
 	public static boolean hasInterface(@NotNull Class<?> clazz, @NotNull Class<?> iface) {
+		Objects.requireNonNull(clazz, "Class must not be null");
 		Objects.requireNonNull(iface, "Interface must not be null");
 		if (iface.isInterface()) {
 			return Lists.newArrayList(clazz.getInterfaces()).contains(iface);
@@ -67,242 +46,149 @@ public class ReflectionHelper {
 		return false;
 	}
 	
-	//region Internal helper methods
-	private static @NotNull List<String> getSimpleNames(Class<?> @NotNull ... classes) {
-		return Lists.newArrayList(classes).stream().map(Class::getSimpleName).collect(Collectors.toList());
-	}
-	
-	private static @NotNull List<String> getSimpleNames(Object @NotNull ... objects) {
-		return Lists.newArrayList(objects).stream().map(Object::getClass).map(Class::getSimpleName).collect(Collectors.toList());
-	}
-	//endregion
-	
 	//region Constructor methods
-	public static <T> Constructor<T> getConstructor(@NotNull Class<T> clazz, Class<?> @NotNull ... parameters) {
+	public static <T> Constructor<T> getConstructor(@NotNull Class<T> clazz, Class<?> @Nullable ... parameters) {
+		Objects.requireNonNull(clazz, "Class must not be null");
 		Constructor<T> constructor = null;
 		try {
-			constructor = Objects.requireNonNull(clazz, "Class must not be null").getDeclaredConstructor(parameters);
+			constructor = clazz.getDeclaredConstructor(nullToEmpty(parameters));
 		} catch (NoSuchMethodException e) {
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
 			LOGGER.warn("Error retrieving the constructor for parameter {} in type '{}'", getSimpleNames(parameters), clazz.getSimpleName());
+			handleException(e);
 		} catch (SecurityException e) {
 			LOGGER.warn("No permission to retrieve constructor of type '{}'", clazz.getSimpleName());
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
-		} catch (NullPointerException e) {
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw e;
-			}
+			handleException(e);
 		}
 		return constructor;
 	}
 	
-	public static boolean hasConstructor(@NotNull Class<?> clazz, Class<?> @NotNull ... parameters) {
+	public static boolean hasConstructor(@NotNull Class<?> clazz, Class<?> @Nullable ... parameters) {
 		return hasConstructor(clazz, null, parameters);
 	}
 	
-	public static <T> boolean hasConstructor(@NotNull Class<T> clazz, @Nullable Predicate<Constructor<T>> predicate, Class<?> @NotNull ... parameters) {
+	public static <T> boolean hasConstructor(@NotNull Class<T> clazz, @Nullable Predicate<Constructor<T>> predicate, Class<?> @Nullable ... parameters) {
+		Objects.requireNonNull(clazz, "Class must not be null");
 		Constructor<T> constructor = null;
 		try {
-			constructor = clazz.getDeclaredConstructor(parameters);
-		} catch (Exception ignored) {
-		
-		}
+			constructor = clazz.getDeclaredConstructor(nullToEmpty(parameters));
+		} catch (Exception ignored) {}
 		return constructor != null && (predicate == null || predicate.test(constructor));
 	}
 	
-	public static <T> T newInstance(@NotNull Constructor<T> constructor, Object @NotNull ... parameters) {
+	public static <T> T newInstance(@NotNull Constructor<T> constructor, Object @Nullable ... parameters) {
+		Objects.requireNonNull(constructor, "Constructor must not be null");
 		T instance = null;
 		try {
-			Objects.requireNonNull(constructor, "Constructor must not be null");
 			if (constructor.trySetAccessible()) {
-				instance = constructor.newInstance(parameters);
+				instance = constructor.newInstance(nullToEmpty(parameters));
 			} else {
 				LOGGER.warn("The package of type '{}' is not accessible for the caller", constructor.getDeclaringClass().getSimpleName());
 			}
 		} catch (InstantiationException e) {
-			LOGGER.warn("Cannot create a new instance of type '{}' with arguments {}", constructor.getDeclaringClass().getSimpleName(), getSimpleNames(parameters));
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
+			LOGGER.warn("Cannot create a new instance of type '{}' with parameters {}", constructor.getDeclaringClass().getSimpleName(), getSimpleNames(parameters));
+			handleException(e);
 		} catch (IllegalAccessException e) {
 			LOGGER.warn("Access to the constructor of type '{}' not possible", constructor.getDeclaringClass().getSimpleName());
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
+			handleException(e);
 		} catch (IllegalArgumentException e) {
 			LOGGER.warn("The parameters {} do not match those of the constructor in type '{}', expected parameters {}", getSimpleNames(parameters), constructor.getDeclaringClass().getSimpleName(), getSimpleNames(constructor.getParameterTypes()));
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
+			handleException(e);
 		} catch (InvocationTargetException e) {
 			LOGGER.warn("Something went wrong when invoking constructor of type '{}' with parameters {}", constructor.getDeclaringClass().getSimpleName(), getSimpleNames(parameters));
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
-		} catch (NullPointerException e) {
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw e;
-			}
+			handleException(e);
 		}
 		return instance;
 	}
 	
-	public static <T> T newInstance(@NotNull Class<T> clazz, Object @NotNull ... parameters) {
-		return newInstance(Objects.requireNonNull(getConstructor(clazz, Lists.newArrayList(parameters).stream().map(Object::getClass).toArray(Class<?>[]::new))), parameters);
+	public static <T> T newInstance(@NotNull Class<T> clazz, Object @Nullable ... parameters) {
+		Objects.requireNonNull(clazz, "Class must not be null");
+		Object[] params = nullToEmpty(parameters);
+		Constructor<T> constructor = getConstructor(clazz, Lists.newArrayList(params).stream().map(Object::getClass).toArray(Class<?>[]::new));
+		if (constructor == null) {
+			throw new IllegalStateException("No constructor for parameters " + getSimpleNames(params) + " in type '" + clazz.getSimpleName() + "' found");
+		}
+		return newInstance(constructor, params);
 	}
 	//endregion
 	
 	//region Method methods
-	public static Method getMethod(@NotNull Class<?> clazz, @NotNull String name, Class<?> @NotNull ... parameters) {
+	public static Method getMethod(@NotNull Class<?> clazz, @NotNull String name, Class<?> @Nullable ... parameters) {
+		Objects.requireNonNull(clazz, "Class must not be null");
+		Objects.requireNonNull(name, "Name must not be null");
 		Method method = null;
 		try {
-			method = Objects.requireNonNull(clazz, "Class must not be null").getDeclaredMethod(name, parameters);
+			method = clazz.getDeclaredMethod(name, nullToEmpty(parameters));
 		} catch (NoSuchMethodException e) {
 			LOGGER.warn("Error retrieving method for name '{}' and parameter {} in type '{}'", name, getSimpleNames(parameters), clazz.getSimpleName());
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
+			handleException(e);
 		} catch (SecurityException e) {
 			LOGGER.warn("No permission to retrieve method with name '{}' and parameters {} in type '{}'", name, getSimpleNames(parameters), clazz.getSimpleName());
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
-		} catch (NullPointerException e) {
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw e;
-			}
+			handleException(e);
 		}
 		return method;
 	}
 	
-	public static boolean hasMethod(@NotNull Class<?> clazz, @NotNull String name, Class<?> @NotNull ... parameters) {
+	public static boolean hasMethod(@NotNull Class<?> clazz, @NotNull String name, Class<?> @Nullable ... parameters) {
 		return hasMethod(clazz, name, null, parameters);
 	}
 	
-	public static boolean hasMethod(@NotNull Class<?> clazz, @NotNull String name, @Nullable Predicate<Method> predicate, Class<?> @NotNull ... parameters) {
+	public static boolean hasMethod(@NotNull Class<?> clazz, @NotNull String name, @Nullable Predicate<Method> predicate, Class<?> @Nullable ... parameters) {
+		Objects.requireNonNull(clazz, "Class must not be null");
+		Objects.requireNonNull(name, "Name must not be null");
 		Method method = null;
 		try {
-			method = clazz.getDeclaredMethod(name, parameters);
-		} catch (Exception ignored) {
-		
-		}
+			method = clazz.getDeclaredMethod(name, nullToEmpty(parameters));
+		} catch (Exception ignored) {}
 		return method != null && (predicate == null || predicate.test(method));
 	}
 	
-	public static Object invoke(@NotNull Method method, @Nullable Object instance, Object @NotNull ... parameters) {
+	public static Object invoke(@NotNull Method method, @Nullable Object instance, Object @Nullable ... parameters) {
+		Objects.requireNonNull(method, "Method must not be null");
 		Object returnValue = null;
 		try {
-			Objects.requireNonNull(method, "Method must not be null");
 			if (method.trySetAccessible()) {
-				returnValue = method.invoke(instance, parameters);
+				returnValue = method.invoke(instance, nullToEmpty(parameters));
 			} else {
 				LOGGER.warn("The package of type '{}' is not accessible for the caller", method.getDeclaringClass().getSimpleName());
 			}
 		} catch (IllegalAccessException e) {
 			LOGGER.warn("Access to the method '{}' in type '{}' and parameter {} not possible", method.getName(), method.getDeclaringClass().getSimpleName(), getSimpleNames(parameters));
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
+			handleException(e);
 		} catch (IllegalArgumentException e) {
 			LOGGER.warn("The parameters {} do not match those of the method '{}' in type '{}', expected parameters {}", getSimpleNames(parameters), method.getName(), method.getDeclaringClass().getSimpleName(), getSimpleNames(method.getParameterTypes()));
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
+			handleException(e);
 		} catch (InvocationTargetException e) {
 			LOGGER.warn("Something went wrong when invoking method '{}' in type '{}' and parameters {}", method.getName(), method.getDeclaringClass().getSimpleName(), getSimpleNames(parameters));
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
-		} catch (NullPointerException e) {
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw e;
-			}
+			handleException(e);
 		}
 		return returnValue;
 	}
 	
-	public static Object invoke(@NotNull Class<?> clazz, @NotNull String name, @Nullable Object instance, Object @NotNull ... parameters) {
-		return invoke(Objects.requireNonNull(getMethod(clazz, name, Lists.newArrayList(parameters).stream().map(Object::getClass).toArray(Class<?>[]::new))), instance, parameters);
+	public static Object invoke(@NotNull Class<?> clazz, @NotNull String name, @Nullable Object instance, Object @Nullable ... parameters) {
+		Objects.requireNonNull(clazz, "Class must not be null");
+		Objects.requireNonNull(name, "Name must not be null");
+		Object[] params = nullToEmpty(parameters);
+		Method method = getMethod(clazz, name, Lists.newArrayList(params).stream().map(Object::getClass).toArray(Class<?>[]::new));
+		if (method == null) {
+			throw new IllegalStateException("No method for name '" + name + "' and parameters " + getSimpleNames(params) + " in type '" + clazz.getSimpleName() + "' found");
+		}
+		return invoke(method, instance, params);
 	}
 	//endregion
 	
 	//region Field methods
 	public static Field getField(@NotNull Class<?> clazz, @NotNull String name) {
+		Objects.requireNonNull(clazz, "Class must not be null");
+		Objects.requireNonNull(name, "Name must not be null");
 		Field field = null;
 		try {
-			field = Objects.requireNonNull(clazz, "Class must not be null").getDeclaredField(name);
+			field = clazz.getDeclaredField(name);
 		} catch (NoSuchFieldException e) {
 			LOGGER.warn("Fail to get field '{}' in type '{}'", name, clazz.getSimpleName());
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
+			handleException(e);
 		} catch (SecurityException e) {
 			LOGGER.warn("No permission to get field '{}' in type '{}'", name, clazz.getSimpleName());
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
-		} catch (NullPointerException e) {
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw e;
-			}
+			handleException(e);
 		}
 		return field;
 	}
@@ -312,19 +198,19 @@ public class ReflectionHelper {
 	}
 	
 	public static boolean hasField(@NotNull Class<?> clazz, @NotNull String name, @Nullable Predicate<Field> predicate) {
+		Objects.requireNonNull(clazz, "Class must not be null");
+		Objects.requireNonNull(name, "Name must not be null");
 		Field field = null;
 		try {
 			field = clazz.getDeclaredField(name);
-		} catch (Exception ignored) {
-		
-		}
+		} catch (Exception ignored) {}
 		return field != null && (predicate == null || predicate.test(field));
 	}
 	
 	public static Object get(@NotNull Field field, @Nullable Object instance) {
+		Objects.requireNonNull(field, "Field must not be null");
 		Object value = null;
 		try {
-			Objects.requireNonNull(field, "Field must not be null");
 			if (field.trySetAccessible()) {
 				value = field.get(instance);
 			} else {
@@ -332,38 +218,27 @@ public class ReflectionHelper {
 			}
 		} catch (IllegalArgumentException e) {
 			LOGGER.warn("Value of the field '{}' in type '{}' cannot be determined", field.getName(), field.getDeclaringClass().getSimpleName());
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
+			handleException(e);
 		} catch (IllegalAccessException e) {
 			LOGGER.warn("Access to field '{}' in type '{}' not possible", field.getName(), field.getDeclaringClass().getSimpleName());
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
-		} catch (NullPointerException e) {
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw e;
-			}
+			handleException(e);
 		}
 		return value;
 	}
 	
 	public static Object get(@NotNull Class<?> clazz, @NotNull String name, @Nullable Object instance) {
-		return get(getField(clazz, name), instance);
+		Objects.requireNonNull(clazz, "Class must not be null");
+		Objects.requireNonNull(name, "Name must not be null");
+		Field field = getField(clazz, name);
+		if (field == null) {
+			throw new IllegalStateException("No field with the name '" + name + "' in type '" + clazz.getSimpleName() + "' found");
+		}
+		return get(field, instance);
 	}
 	
 	public static void set(@NotNull Field field, @Nullable Object instance, @Nullable Object value) {
+		Objects.requireNonNull(field, "Field must not be null");
 		try {
-			Objects.requireNonNull(field, "Field must not be null");
 			if (field.trySetAccessible()) {
 				field.set(instance, value);
 			} else {
@@ -371,32 +246,38 @@ public class ReflectionHelper {
 			}
 		} catch (IllegalArgumentException e) {
 			LOGGER.warn("Value of the field with the name '{}' in type '{}' could not be set", field.getName(), field.getDeclaringClass().getSimpleName());
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
+			handleException(e);
 		} catch (IllegalAccessException e) {
 			LOGGER.warn("Can not access field '{}' in type '{}'", field.getName(), field.getDeclaringClass().getSimpleName());
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw new RuntimeException(e);
-			}
-		} catch (NullPointerException e) {
-			if (logExceptions) {
-				LOGGER.error(e);
-			}
-			if (throwExceptions) {
-				throw e;
-			}
+			handleException(e);
 		}
 	}
 	
 	public static void set(@NotNull Class<?> clazz, @NotNull String name, @Nullable Object instance, @Nullable Object value) {
-		set(getField(clazz, name), instance, value);
+		Objects.requireNonNull(clazz, "Class must not be null");
+		Objects.requireNonNull(name, "Name must not be null");
+		Field field = getField(clazz, name);
+		if (field == null) {
+			throw new IllegalStateException("No field with the name '" + name + "' in type '" + clazz.getSimpleName() + "' found");
+		}
+		set(field, instance, value);
+	}
+	//region Internal
+	private static void handleException(@NotNull Exception e) {
+		if (Boolean.parseBoolean(System.getProperty(REFLECTION_EXCEPTIONS_LOG, "false"))) {
+			LOGGER.error(e);
+		}
+		if (Boolean.parseBoolean(System.getProperty(REFLECTION_EXCEPTIONS_THROW, "false"))) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private static @NotNull List<String> getSimpleNames(Class<?> @Nullable ... classes) {
+		return Lists.newArrayList(nullToEmpty(classes)).stream().map(Class::getSimpleName).collect(Collectors.toList());
+	}
+	
+	private static @NotNull List<String> getSimpleNames(Object @Nullable ... objects) {
+		return Lists.newArrayList(nullToEmpty(objects)).stream().map(Object::getClass).map(Class::getSimpleName).collect(Collectors.toList());
 	}
 	//endregion
 }
