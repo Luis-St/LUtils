76ad9b804fcf1c8879f4dd3e4f4d5204f7efd023
Luis <luis@staudt-fridingen.de>
2024-02-03 21:18:28 +0100

    added javadocs to SimpleCell

--- src/main/java/net/luis/utils/util/SimpleCell.java
+++ src/main/java/net/luis/utils/util/SimpleCell.java
@@ -1,19 +1,32 @@
 package net.luis.utils.util;
 
-import com.google.common.collect.Table.Cell;
+import com.google.common.collect.Table;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 
 /**
+ * A simple implementation of the {@link Table.Cell} interface.<br>
  *
  * @author Luis-St
  *
+ * @param getRowKey The row key of the cell
+ * @param getColumnKey The column key of the cell
+ * @param getValue The value of the cell
+ * @param <R> The type of the row key
+ * @param <C> The type of the column key
+ * @param <V> The type of the value
  */
-
-public record SimpleCell<R, C, V>(@NotNull R getRowKey, @NotNull C getColumnKey, @Nullable V getValue) implements Cell<R, C, V> {
+public record SimpleCell<R, C, V>(@NotNull R getRowKey, @NotNull C getColumnKey, @Nullable V getValue) implements Table.Cell<R, C, V> {
 	
+	/**
+	 * Constructs a new {@link SimpleCell} with the specified row key, column key and value.<br>
+	 * @param getRowKey The row key of the cell
+	 * @param getColumnKey The column key of the cell
+	 * @param getValue The value of the cell
+	 * @throws NullPointerException if the row key or column key is null
+	 */
 	public SimpleCell {
 		Objects.requireNonNull(getRowKey, "Row key must not be null");
 		Objects.requireNonNull(getColumnKey, "Column key must not be null");
