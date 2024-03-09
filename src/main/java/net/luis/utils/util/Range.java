5a5ed0c053a30af9301dce4299158a82b4455f0b
Luis <luis@staudt-fridingen.de>
2024-03-09 20:24:23 +0100

    added expand and parse method to Range

--- src/main/java/net/luis/utils/util/Range.java
+++ src/main/java/net/luis/utils/util/Range.java
@@ -19,7 +19,9 @@
 package net.luis.utils.util;
 
 import net.luis.utils.math.Mth;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 
@@ -31,6 +33,12 @@ import java.util.Objects;
 public class Range {
 	
 	/**
+	 * A constant for an empty range.<br>
+	 * The minimum and maximum value are both 0.<br>
+	 */
+	public static final Range EMPTY = new Range(0, 0);
+	
+	/**
 	 * The minimum value of the range.<br>
 	 */
 	private final double min;
@@ -40,7 +48,7 @@ public class Range {
 	private final double max;
 	
 	/**
-	 * Constructs a new {@link Range} with the specified minimum and maximum value.<br>
+	 * Constructs a new range with the specified minimum and maximum value.<br>
 	 * @param min The minimum value of the range
 	 * @param max The maximum value of the range
 	 * @throws IllegalArgumentException If the maximum value is less than the minimum value
@@ -54,43 +62,66 @@ public class Range {
 	}
 	
 	/**
-	 * Creates a new {@link Range} with the specified maximum value.<br>
+	 * Creates a new range with the specified maximum value.<br>
 	 * The minimum value is set to 0.<br>
 	 * @param max The maximum value of the range
-	 * @return The created range
-	 * @throws IllegalArgumentException If the maximum value is less than 0
+	 * @return The created range or an empty range if the maximum value is less than 0
+	 * @see #of(double, double)
 	 */
 	public static @NotNull Range of(double max) {
-		return of(0, max);
+		return of(0.0, max);
 	}
 	
 	/**
-	 * Creates a new {@link Range} with the specified minimum and maximum value.<br>
-	 * The characters are converted to their respective doubleeger values.<br>
+	 * Creates a new range with the specified minimum and maximum value.<br>
+	 * The characters are converted to their respective double values.<br>
 	 * @param min The minimum value of the range
 	 * @param max The maximum value of the range
-	 * @return The created range
-	 * @throws IllegalArgumentException If the maximum value is less than the minimum value
+	 * @return The created range or an empty range if the maximum value is less than the minimum value
+	 * @see #of(double, double)
 	 */
 	public static @NotNull Range of(char min, char max) {
 		return of(min, (double) max);
 	}
 	
 	/**
-	 * Creates a new {@link Range} with the specified minimum and maximum value.<br>
+	 * Creates a new range with the specified minimum and maximum value.<br>
 	 * @param min The minimum value of the range
 	 * @param max The maximum value of the range
-	 * @return The created range
-	 * @throws IllegalArgumentException If the maximum value is less than the minimum value
+	 * @return The created range or an empty range if the maximum value is less than the minimum value
+	 * @see #EMPTY
 	 */
 	public static @NotNull Range of(double min, double max) {
+		if (min > max) {
+			return EMPTY;
+		}
 		return new Range(min, max);
+	}
+	
+	/**
+	 * Parses the specified string to a range.<br>
+	 * The string must be in the format of {@code [min..max]}.<br>
+	 * @param str The string to parse
+	 * @return The parsed range, or an empty range if the string is invalid
+	 */
+	public static @NotNull Range parse(@Nullable String str) {
+		if (StringUtils.isBlank(str)) {
+			return EMPTY;
+		}
+		if (!str.startsWith("[") || !str.endsWith("]") || !str.contains("..")) {
+			return EMPTY;
+		}
+		String[] parts = str.substring(1, str.length() - 1).split("\\.\\.");
+		if (parts.length != 2) {
+			return EMPTY;
+		}
+		return of(Double.parseDouble(parts[0].strip()), Double.parseDouble(parts[1].strip()));
 	}
 	
 	/**
 	 * @return The minimum value of the range
 	 */
-	public Number getMin() {
+	public double getMin() {
 		return this.min;
 	}
 	
@@ -106,6 +137,48 @@ public class Range {
 	 */
 	public double getRange() {
 		return this.max - this.min;
+	}
+	
+	/**
+	 * Moves the range by the specified value.<br>
+	 * The minimum and maximum value are both increased by the specified value.<br>
+	 * @param value The value to move the range by
+	 * @return The moved range
+	 */
+	public @NotNull Range move(double value) {
+		return of(this.min + value, this.max + value);
+	}
+	
+	/**
+	 * Expands the range by the specified value.<br>
+	 * <p>
+	 *     The minimum value is decreased by the absolute of the specified value<br>
+	 *     and the maximum value is increased by the absolute of specified value.<br>
+	 * </p>
+	 * @param value The value to expand the range by
+	 * @return The expanded range
+	 */
+	public @NotNull Range expand(double value) {
+		return of(this.min - Math.abs(value), this.max + Math.abs(value));
+	}
+	
+	/**
+	 * Expands the maximum value of the range by the absolute of specified value.<br>
+	 * @param value The value to expand the maximum value by
+	 * @return The expanded range
+	 */
+	public @NotNull Range expandMax(double value) {
+		return of(this.min, this.max + Math.abs(value));
+	}
+	
+	/**
+	 * Expands the minimum value of the range by the absolute of specified value.<br>
+	 * The resulting range is maybe empty if the given value is negative.<br>
+	 * @param value The value to expand the minimum value by
+	 * @return The expanded range
+	 */
+	public @NotNull Range expandMin(double value) {
+		return of(this.min - Math.abs(value), this.max);
 	}
 	
 	/**
@@ -135,7 +208,7 @@ public class Range {
 	
 	@Override
 	public String toString() {
-		return "[" + this.min + "; " + this.max + "]";
+		return "[" + this.min + ".." + this.max + "]";
 	}
 	//endregion
 }
--- src/test/java/net/luis/utils/util/RangeTest.java
+++ src/test/java/net/luis/utils/util/RangeTest.java
@@ -31,10 +31,23 @@ class RangeTest {
 	
 	@Test
 	void of() {
+		assertDoesNotThrow(() -> Range.of(9.0, 0.0));
+		assertEquals(Range.EMPTY, Range.of(9.0, 0.0));
 		assertNotNull(Range.of(9.0));
 		assertNotNull(Range.of(0.0, 0));
 		assertNotNull(Range.of(0.0, 9));
-		assertThrows(IllegalArgumentException.class, () -> Range.of(9.0, 0.0));
+	}
+	
+	@Test
+	void parse() {
+		assertDoesNotThrow(() -> Range.parse(null));
+		assertEquals(Range.EMPTY, Range.parse(""));
+		assertEquals(Range.EMPTY, Range.parse(" "));
+		assertEquals(Range.EMPTY, Range.parse("0..1"));
+		assertEquals(Range.EMPTY, Range.parse("[0;1]"));
+		assertEquals(Range.EMPTY, Range.parse("[0..1..2]"));
+		assertEquals(Range.of(0, 1), Range.parse("[0..1]"));
+		assertEquals(Range.of(0.05, 0.95), Range.parse("[0.05..0.95]"));
 	}
 	
 	@Test
@@ -47,6 +60,41 @@ class RangeTest {
 	void getMax() {
 		assertEquals(9.0, Range.of(0.0, 9.0).getMax());
 		assertNotEquals(8.0, Range.of(0.0, 9.0).getMax());
+	}
+	
+	@Test
+	void getRange() {
+		assertEquals(9.0, Range.of(0.0, 9.0).getRange());
+		assertEquals(9.0, Range.of(-9.0, 0.0).getRange());
+		assertNotEquals(8.0, Range.of(0.0, 9.0).getRange());
+	}
+	
+	@Test
+	void move() {
+		assertEquals(Range.of(0.0, 9.0), Range.of(0.0, 9.0).move(0.0));
+		assertEquals(Range.of(1.0, 10.0), Range.of(0.0, 9.0).move(1.0));
+		assertEquals(Range.of(-1.0, 8.0), Range.of(0.0, 9.0).move(-1.0));
+	}
+	
+	@Test
+	void expand() {
+		assertEquals(Range.of(0.0, 9.0), Range.of(0.0, 9.0).expand(0.0));
+		assertEquals(Range.of(-1.0, 10.0), Range.of(0.0, 9.0).expand(1.0));
+		assertEquals(Range.of(-1.0, 10.0), Range.of(0.0, 9.0).expand(-1.0));
+	}
+	
+	@Test
+	void expandMax() {
+		assertEquals(Range.of(0.0, 9.0), Range.of(0.0, 9.0).expandMax(0.0));
+		assertEquals(Range.of(0.0, 10.0), Range.of(0.0, 9.0).expandMax(1.0));
+		assertEquals(Range.of(0.0, 10.0), Range.of(0.0, 9.0).expandMax(-1.0));
+	}
+	
+	@Test
+	void expandMin() {
+		assertEquals(Range.of(0.0, 9.0), Range.of(0.0, 9.0).expandMin(0.0));
+		assertEquals(Range.of(-1.0, 9.0), Range.of(0.0, 9.0).expandMin(1.0));
+		assertEquals(Range.of(-1.0, 9.0), Range.of(0.0, 9.0).expandMin(-1.0));
 	}
 	
 	@Test
