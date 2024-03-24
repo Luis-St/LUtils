a46b7d8618e0a99ef4d50b9b2dac9120795937a3
Luis <luis@staudt-fridingen.de>
2024-03-24 13:44:37 +0100

    added key and key type

---
+++ src/main/java/net/luis/utils/data/key/KeyType.java
@@ -0,0 +1,102 @@
+/*
+ * LUtils
+ * Copyright (C) 2024 Luis Staudt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.luis.utils.data.key;
+
+import com.google.common.collect.Lists;
+import net.luis.utils.annotation.ReflectiveUsage;
+import net.luis.utils.lang.EnumLike;
+import net.luis.utils.util.Priority;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ *
+ * @author Luis-St
+ *
+ */
+
+public class KeyType<K extends Key> implements EnumLike<KeyType<?>> {
+	
+	@ReflectiveUsage
+	@SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+	private static final List<KeyType<?>> VALUES = Lists.newLinkedList();
+	
+	
+	private final String name;
+	private final Priority priority;
+	private final Predicate<String> validator;
+	private final Function<String, K> factory;
+	
+	public KeyType(@NotNull String name, @NotNull Priority priority, @NotNull Predicate<String> validator, @NotNull Function<String, K> factory) {
+		this.name = Objects.requireNonNull(name, "Name must not be null");
+		this.priority = Objects.requireNonNull(priority, "Priority must not be null");
+		this.validator = Objects.requireNonNull(validator, "Validator must not be null");
+		this.factory = Objects.requireNonNull(factory, "Factory must not be null");
+		VALUES.add(this);
+	}
+	
+	public @NotNull String name() {
+		return this.name;
+	}
+	
+	public @NotNull Priority getPriority() {
+		return this.priority;
+	}
+	
+	public boolean isValid(@Nullable String key) {
+		return this.validator.test(key);
+	}
+	
+	public @NotNull K createKey(@NotNull String key) {
+		return this.factory.apply(key);
+	}
+	
+	@Override
+	public int compareTo(@NotNull KeyType<?> object) {
+		return this.priority.compareTo(object.priority);
+	}
+	
+	//region Object overrides
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof KeyType<?> keyType)) return false;
+		
+		if (!this.name.equals(keyType.name)) return false;
+		if (!this.priority.equals(keyType.priority)) return false;
+		if (!this.validator.equals(keyType.validator)) return false;
+		return this.factory.equals(keyType.factory);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.name, this.priority, this.validator, this.factory);
+	}
+	
+	@Override
+	public String toString() {
+		return "KeyType: " + this.name + " (" + this.priority + ")";
+	}
+	//endregion
+}
