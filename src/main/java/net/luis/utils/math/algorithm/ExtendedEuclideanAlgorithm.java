cd39ce2da667e61c11e6da813e3b42170d106cf2
Luis <luis@staudt-fridingen.de>
2024-07-10 21:42:38 +0200

    added javadocs to the eea

--- src/main/java/net/luis/utils/math/algorithm/ExtendedEuclideanAlgorithm.java
+++ src/main/java/net/luis/utils/math/algorithm/ExtendedEuclideanAlgorithm.java
@@ -26,31 +26,76 @@ import java.util.*;
 import static java.math.BigInteger.*;
 
 /**
+ * Implementation of the Extended Euclidean Algorithm (EEA).<br>
+ * The EEA is an extension of the Euclidean Algorithm and is used to find the greatest common divisor (GCD) of two integers.<br>
  *
  * @author Luis-St
- *
  */
-
 public class ExtendedEuclideanAlgorithm {
 	
+	/**
+	 * The first coefficients of the EEA.<br>
+	 */
 	private final List<BigInteger> firstCoefficients = new ArrayList<>();
+	/**
+	 * The second coefficients of the EEA.<br>
+	 */
 	private final List<BigInteger> secondCoefficients = new ArrayList<>();
+	/**
+	 * The initial value of the EEA.<br>
+	 */
 	private final BigInteger initialValue;
+	/**
+	 * The initial divisor of the EEA.<br>
+	 */
 	private final BigInteger initialDivisor;
+	/**
+	 * The current value of the EEA.<br>
+	 */
 	private BigInteger value;
+	/**
+	 * The current divisor of the EEA.<br>
+	 */
 	private BigInteger divisor;
+	/**
+	 * The current remainder of the EEA.<br>
+	 */
 	private BigInteger remainder;
+	/**
+	 * The current quotient of the EEA.<br>
+	 */
 	private BigInteger quotient;
+	/**
+	 * The number of steps the EEA has executed.<br>
+	 */
 	private int step;
 	
+	/**
+	 * Constructs a new EEA with the given value and divisor.<br>
+	 * @param value The value of the EEA
+	 * @param divisor The divisor of the EEA
+	 * @throws IllegalArgumentException If the value or divisor is 0
+	 */
 	public ExtendedEuclideanAlgorithm(int value, int divisor) {
 		this(BigInteger.valueOf(value), BigInteger.valueOf(divisor));
 	}
 	
+	/**
+	 * Constructs a new EEA with the given value and divisor.<br>
+	 * @param value The value of the EEA
+	 * @param divisor The divisor of the EEA
+	 * @throws IllegalArgumentException If the value or divisor is 0
+	 */
 	public ExtendedEuclideanAlgorithm(long value, long divisor) {
 		this(BigInteger.valueOf(value), BigInteger.valueOf(divisor));
 	}
 	
+	/**
+	 * Constructs a new EEA with the given value and divisor.<br>
+	 * @param value The value of the EEA
+	 * @param divisor The divisor of the EEA
+	 * @throws IllegalArgumentException If the value or divisor is 0
+	 */
 	public ExtendedEuclideanAlgorithm(@NotNull BigInteger value, @NotNull BigInteger divisor) {
 		//region Validation
 		if (value.equals(ZERO)) {
@@ -70,14 +115,35 @@ public class ExtendedEuclideanAlgorithm {
 		this.secondCoefficients.addAll(List.of(ZERO, ONE));
 	}
 	
+	/**
+	 * Calculates the greatest common divisor (GCD) of the given value and divisor using the EEA.<br>
+	 * @param value The value to calculate the GCD for
+	 * @param divisor The divisor to calculate the GCD for
+	 * @return The GCD of the given value and divisor
+	 * @throws IllegalArgumentException If the value or divisor is 0
+	 */
 	public static int gcd(int value, int divisor) {
 		return gcd(BigInteger.valueOf(value), BigInteger.valueOf(divisor)).intValueExact();
 	}
 	
+	/**
+	 * Calculates the greatest common divisor (GCD) of the given value and divisor using the EEA.<br>
+	 * @param value The value to calculate the GCD for
+	 * @param divisor The divisor to calculate the GCD for
+	 * @return The GCD of the given value and divisor
+	 * @throws IllegalArgumentException If the value or divisor is 0
+	 */
 	public static long gcd(long value, long divisor) {
 		return gcd(BigInteger.valueOf(value), BigInteger.valueOf(divisor)).longValueExact();
 	}
 	
+	/**
+	 * Calculates the greatest common divisor (GCD) of the given value and divisor using the EEA.<br>
+	 * @param value The value to calculate the GCD for
+	 * @param divisor The divisor to calculate the GCD for
+	 * @return The GCD of the given value and divisor
+	 * @throws IllegalArgumentException If the value or divisor is 0
+	 */
 	public static @NotNull BigInteger gcd(@NotNull BigInteger value, @NotNull BigInteger divisor) {
 		ExtendedEuclideanAlgorithm eea = new ExtendedEuclideanAlgorithm(value, divisor);
 		eea.execute();
@@ -85,51 +151,91 @@ public class ExtendedEuclideanAlgorithm {
 	}
 	
 	//region Getter
+	
+	/**
+	 * @return True, if the EEA is complete, otherwise false
+	 */
 	public boolean isComplete() {
 		return this.remainder.equals(ZERO);
 	}
 	
+	/**
+	 * @return The initial value of the EEA
+	 */
 	public @NotNull BigInteger getInitialValue() {
 		return this.initialValue;
 	}
 	
+	/**
+	 * @return The initial divisor of the EEA
+	 */
 	public @NotNull BigInteger getInitialDivisor() {
 		return this.initialDivisor;
 	}
 	
+	/**
+	 * @return The current value of the EEA
+	 */
 	public @NotNull BigInteger getValue() {
 		return this.value;
 	}
 	
+	/**
+	 * @return The current divisor of the EEA
+	 */
 	public @NotNull BigInteger getDivisor() {
 		return this.divisor;
 	}
 	
+	/**
+	 * @return The current remainder of the EEA
+	 */
 	public @NotNull BigInteger getRemainder() {
 		return this.remainder;
 	}
 	
+	/**
+	 * @return The current quotient of the EEA
+	 */
 	public @NotNull BigInteger getQuotient() {
 		return this.quotient;
 	}
 	
+	/**
+	 * @return The first coefficient of the EEA
+	 */
 	public int getStep() {
 		return this.step;
 	}
 	
+	/**
+
+	 * @return The current first coefficient of the EEA
+	 */
 	public @NotNull BigInteger getFirstCoefficient() {
 		return this.getFirstCoefficient(1);
 	}
 	
+	/**
+	 * @return The current second coefficient of the EEA
+	 */
 	public @NotNull BigInteger getSecondCoefficient() {
 		return this.getSecondCoefficient(1);
 	}
 	//endregion
 	
+	/**
+	 * Executes the EEA for a single step.<br>
+	 */
 	public void execute() {
 		this.execute(1);
 	}
 	
+	/**
+	 * Executes the EEA for the given number of steps.<br>
+	 * If the EEA is already completed the method will return immediately.<br>
+	 * @param steps The number of steps to execute
+	 */
 	public void execute(int steps) {
 		if (this.isComplete() || steps <= 0) {
 			return;
@@ -139,6 +245,9 @@ public class ExtendedEuclideanAlgorithm {
 		}
 	}
 	
+	/**
+	 * Executes the EEA until it is complete.<br>
+	 */
 	public void executeUntilComplete() {
 		while (!this.isComplete()) {
 			this.executeStep();
@@ -146,14 +255,32 @@ public class ExtendedEuclideanAlgorithm {
 	}
 	
 	//region Helper methods
+	
+	/**
+	 * Gets the first coefficient of the EEA.<br>
+	 * The number of steps is subtracted from the size of the first coefficients list to get the correct coefficient.<br>
+	 * @param step The number of steps to go back
+	 * @return The first coefficient of the EEA
+	 */
 	private @NotNull BigInteger getFirstCoefficient(int step) {
 		return this.firstCoefficients.get(this.firstCoefficients.size() - Math.abs(step));
 	}
 	
+	/**
+	 * Gets the second coefficient of the EEA.<br>
+	 * The number of steps is subtracted from the size of the second coefficients list to get the correct coefficient.<br>
+	 * @param step The number of steps to go back
+	 * @return The second coefficient of the EEA
+	 */
 	private @NotNull BigInteger getSecondCoefficient(int step) {
 		return this.secondCoefficients.get(this.secondCoefficients.size() - Math.abs(step));
 	}
 	
+	/**
+	 * Executes a single step of the EEA.<br>
+	 * The values of the EEA are recalculated.<br>
+	 * @see #update(BigInteger, BigInteger, BigInteger, BigInteger)
+	 */
 	private void executeStep() {
 		BigInteger base = this.divisor;
 		BigInteger divisor = this.remainder;
@@ -162,6 +289,15 @@ public class ExtendedEuclideanAlgorithm {
 		this.update(base, divisor, remainder, quotient);
 	}
 	
+	/**
+	 * Updates the values of the EEA.<br>
+	 * The first and second coefficients are calculated and added to their respective lists.<br>
+	 * The value, divisor, remainder, quotient and step are updated.<br>
+	 * @param value The new value
+	 * @param divisor The new divisor
+	 * @param remainder The new remainder
+	 * @param quotient The new quotient
+	 */
 	private void update(@NotNull BigInteger value, @NotNull BigInteger divisor, @NotNull BigInteger remainder, @NotNull BigInteger quotient) {
 		BigInteger first = this.getFirstCoefficient(-2).subtract(this.quotient.multiply(this.getFirstCoefficient(-1)));
 		BigInteger second = this.getSecondCoefficient(-2).subtract(this.quotient.multiply(this.getSecondCoefficient(-1)));
