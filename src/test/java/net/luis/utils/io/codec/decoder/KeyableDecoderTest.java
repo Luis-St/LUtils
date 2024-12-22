cfa2dd5c0abc647c63fa14a5297800623b3b6d1e
Luis <luis@staudt-fridingen.de>
2024-12-22 18:26:05 +0100

    added tests for keyable encoder and decoder

---
+++ src/test/java/net/luis/utils/io/codec/decoder/KeyableDecoderTest.java
@@ -0,0 +1,57 @@
+/*
+ * LUtils
+ * Copyright (C) 2024 Luis Staudt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.luis.utils.io.codec.decoder;
+
+import net.luis.utils.io.codec.Codec;
+import net.luis.utils.io.codec.provider.JsonTypeProvider;
+import net.luis.utils.util.Result;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for {@link KeyableDecoder}.<br>
+ *
+ * @author Luis-St
+ */
+class KeyableDecoderTest {
+	
+	@Test
+	void of() {
+		assertThrows(NullPointerException.class, () -> KeyableDecoder.of(null, Integer::valueOf));
+		assertThrows(NullPointerException.class, () -> KeyableDecoder.of(Codec.INTEGER, null));
+		assertNotNull(KeyableDecoder.of(Codec.INTEGER, Integer::valueOf));
+	}
+	
+	@Test
+	void decodeKey() {
+		JsonTypeProvider typeProvider = JsonTypeProvider.INSTANCE;
+		KeyableDecoder<Integer>  decoder = Codec.INTEGER;
+		
+		assertThrows(NullPointerException.class, () -> decoder.decodeKey(null, "1"));
+		assertThrows(NullPointerException.class, () -> decoder.decodeKey(typeProvider, null));
+		
+		Result<Integer> invalidResult = decoder.decodeKey(typeProvider, "invalid");
+		assertTrue(invalidResult.isError());
+		
+		Result<Integer> result = decoder.decodeKey(typeProvider, "1");
+		assertTrue(result.isSuccess());
+		assertEquals(1, result.orThrow());
+	}
+}
---
+++ src/test/java/net/luis/utils/io/codec/encoder/KeyableEncoderTest.java
@@ -0,0 +1,54 @@
+/*
+ * LUtils
+ * Copyright (C) 2024 Luis Staudt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.luis.utils.io.codec.encoder;
+
+import net.luis.utils.io.codec.Codec;
+import net.luis.utils.io.codec.provider.JsonTypeProvider;
+import net.luis.utils.util.Result;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for {@link KeyableEncoder}.<br>
+ *
+ * @author Luis-St
+ */
+class KeyableEncoderTest {
+	
+	@Test
+	void of() {
+		assertThrows(NullPointerException.class, () -> KeyableEncoder.of(null, String::valueOf));
+		assertThrows(NullPointerException.class, () -> KeyableEncoder.of(Codec.INTEGER, null));
+		assertNotNull(KeyableEncoder.of(Codec.INTEGER, String::valueOf));
+	}
+	
+	@Test
+	void encodeKey() {
+		JsonTypeProvider typeProvider = JsonTypeProvider.INSTANCE;
+		KeyableEncoder<Integer> decoder = Codec.INTEGER;
+		
+		assertThrows(NullPointerException.class, () -> decoder.encodeKey(null, 1));
+		assertThrows(NullPointerException.class, () -> decoder.encodeKey(typeProvider, null));
+		
+		Result<String> result = decoder.encodeKey(typeProvider, 1);
+		assertTrue(result.isSuccess());
+		assertEquals("1", result.orThrow());
+	}
+}
