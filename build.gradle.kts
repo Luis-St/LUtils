14d99611b314f20e318116ea8be5e198387e7b71
Luis <luis@staudt-fridingen.de>
2025-03-27 14:38:31 +0100

    updated to version 7.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,7 @@ publishing {
 		create<MavenPublication>("mavenJava") {
 			groupId = "net.luis"
 			artifactId = "LUtils"
-			version = "7.2.0"
+			version = "7.3.0"
 			artifact(tasks.named<Jar>("jar"))
 			artifact(tasks.named<Jar>("sourcesJar"))
 			artifact(tasks.named<Jar>("javadocJar"))
