0a77c3cd2e553c1e07aae2d4f206aac06e543a26
Luis <luis@staudt-fridingen.de>
2025-05-03 10:42:09 +0200

    added always match and end token rule

---
+++ src/main/java/net/luis/utils/io/token/rule/rules/AlwaysMatchTokenRule.java
@@ -0,0 +1,52 @@
+/*
+ * LUtils
+ * Copyright (C) 2025 Luis Staudt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.luis.utils.io.token.rule.rules;
+
+import net.luis.utils.annotation.type.Singleton;
+import net.luis.utils.io.token.rule.TokenRuleMatch;
+import net.luis.utils.io.token.tokens.Token;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+
+/**
+ *
+ * @author Luis-St
+ *
+ */
+
+@Singleton
+public final class AlwaysMatchTokenRule implements TokenRule {
+	
+	public static final AlwaysMatchTokenRule INSTANCE = new AlwaysMatchTokenRule();
+	
+	private AlwaysMatchTokenRule() {}
+	
+	@Override
+	public @Nullable TokenRuleMatch match(@NotNull List<Token> tokens, int startIndex) {
+		Objects.requireNonNull(tokens, "Tokens must not be null");
+		if (startIndex >= tokens.size()) {
+			return null;
+		}
+		
+		List<Token> matchedTokens = Collections.singletonList(tokens.get(startIndex));
+		return new TokenRuleMatch(startIndex, startIndex + 1, matchedTokens, this);
+	}
+}
