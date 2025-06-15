fb9e302260ff90ca6932752e613cd0f9810b248c
Luis <luis@staudt-fridingen.de>
2025-06-16 00:08:21 +0200

    updated to linux line endings

--- src/main/java/net/luis/utils/io/codec/mapping/CodecComponent.java
+++ src/main/java/net/luis/utils/io/codec/mapping/CodecComponent.java
@@ -1,8 +1,27 @@
+/*
+ * LUtils
+ * Copyright (C) 2025 Luis Staudt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package net.luis.utils.io.codec.mapping;
 
 import net.luis.utils.util.Either;
 import net.luis.utils.util.unsafe.reflection.ReflectionHelper;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnknownNullability;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
@@ -42,7 +61,7 @@ public class CodecComponent {
 		);
 	}
 	
-	public <T extends Annotation> T getAnnotation(@NotNull Class<T> annotationClass) {
+	public <T extends Annotation> @UnknownNullability T getAnnotation(@NotNull Class<T> annotationClass) {
 		return this.component.mapTo(
 			component -> component.getAnnotation(annotationClass),
 			field -> field.getAnnotation(annotationClass)
