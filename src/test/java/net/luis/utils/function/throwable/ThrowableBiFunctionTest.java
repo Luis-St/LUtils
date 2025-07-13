9fad29990d3c1d2d2adf469ebfec3098706683da
Luis <luis@staudt-fridingen.de>
2025-07-13 20:47:01 +0200

    improved function tests

--- src/test/java/net/luis/utils/function/throwable/ThrowableBiFunctionTest.java
+++ src/test/java/net/luis/utils/function/throwable/ThrowableBiFunctionTest.java
@@ -20,6 +20,8 @@ package net.luis.utils.function.throwable;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.BiFunction;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -30,39 +32,196 @@ import static org.junit.jupiter.api.Assertions.*;
 class ThrowableBiFunctionTest {
 	
 	@Test
-	void caught() {
+	void caughtConvertsSuccessfulFunction() {
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> function = (a, b) -> a + b;
+		BiFunction<Integer, Integer, Integer> caught = ThrowableBiFunction.caught(function);
+		
+		assertEquals(5, caught.apply(2, 3));
+		assertDoesNotThrow(() -> caught.apply(1, 1));
+	}
+	
+	@Test
+	void caughtWrapsExceptionInRuntimeException() {
 		ThrowableBiFunction<Integer, Integer, Integer, Exception> function = (a, b) -> {
-			if (a != 1 || b != 2) {
-				throw new Exception();
-			}
-			assertEquals(1, a);
-			assertEquals(2, b);
-			return a + b;
+			throw new Exception("Test exception");
 		};
-		assertEquals(3, assertDoesNotThrow(() -> ThrowableBiFunction.caught(function).apply(1, 2)));
-		assertThrows(RuntimeException.class, () -> ThrowableBiFunction.caught(function).apply(2, 2));
+		BiFunction<Integer, Integer, Integer> caught = ThrowableBiFunction.caught(function);
+		
+		RuntimeException exception = assertThrows(RuntimeException.class, () -> caught.apply(1, 2));
+		assertInstanceOf(Exception.class, exception.getCause());
+		assertEquals("Test exception", exception.getCause().getMessage());
+	}
+	
+	@Test
+	void caughtWithNullFunction() {
 		assertThrows(NullPointerException.class, () -> ThrowableBiFunction.caught(null));
 	}
 	
 	@Test
-	void apply() {
-		ThrowableBiFunction<Integer, Integer, Integer, Exception> function = (a, b) -> {
-			if (a != 1 || b != 2) {
-				throw new Exception();
-			}
-			assertEquals(1, a);
-			assertEquals(2, b);
-			return a + b;
-		};
-		assertEquals(3, assertDoesNotThrow(() -> function.apply(1, 2)));
-		assertThrows(Exception.class, () -> function.apply(2, 2));
+	void caughtWithDifferentParameterTypes() {
+		ThrowableBiFunction<String, Integer, String, Exception> function = (s, i) -> s + i;
+		BiFunction<String, Integer, String> caught = ThrowableBiFunction.caught(function);
+		
+		assertEquals("test5", caught.apply("test", 5));
 	}
 	
 	@Test
-	void andThen() {
+	void caughtWithNullParameters() {
+		ThrowableBiFunction<String, String, String, Exception> function = (a, b) ->
+			(a == null ? "null" : a) + (b == null ? "null" : b);
+		BiFunction<String, String, String> caught = ThrowableBiFunction.caught(function);
+		
+		assertEquals("nullnull", caught.apply(null, null));
+		assertEquals("anull", caught.apply("a", null));
+		assertEquals("nullb", caught.apply(null, "b"));
+	}
+	
+	@Test
+	void caughtWithNullResult() {
+		ThrowableBiFunction<String, String, String, Exception> function = (a, b) -> null;
+		BiFunction<String, String, String> caught = ThrowableBiFunction.caught(function);
+		
+		assertNull(caught.apply("a", "b"));
+	}
+	
+	@Test
+	void applyWithSuccessfulExecution() {
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> function = (a, b) -> a * b;
+		
+		assertEquals(6, assertDoesNotThrow(() -> function.apply(2, 3)));
+		assertEquals(0, assertDoesNotThrow(() -> function.apply(0, 5)));
+		assertEquals(-10, assertDoesNotThrow(() -> function.apply(-2, 5)));
+	}
+	
+	@Test
+	void applyWithException() {
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> function = (a, b) -> {
+			if (a < 0 || b < 0) {
+				throw new Exception("Negative values not allowed");
+			}
+			return a + b;
+		};
+		
+		assertEquals(7, assertDoesNotThrow(() -> function.apply(3, 4)));
+		
+		Exception exception = assertThrows(Exception.class, () -> function.apply(-1, 2));
+		assertEquals("Negative values not allowed", exception.getMessage());
+		
+		assertThrows(Exception.class, () -> function.apply(1, -2));
+	}
+	
+	@Test
+	void applyWithNullParameters() {
+		ThrowableBiFunction<String, String, String, Exception> function = (a, b) -> {
+			if (a == null || b == null) {
+				throw new Exception("Null parameters not allowed");
+			}
+			return a + b;
+		};
+		
+		assertEquals("ab", assertDoesNotThrow(() -> function.apply("a", "b")));
+		assertThrows(Exception.class, () -> function.apply(null, "b"));
+		assertThrows(Exception.class, () -> function.apply("a", null));
+		assertThrows(Exception.class, () -> function.apply(null, null));
+	}
+	
+	@Test
+	void applyWithDifferentTypes() {
+		ThrowableBiFunction<String, Integer, Boolean, Exception> function = (s, i) -> s.length() == i;
+		
+		assertTrue(assertDoesNotThrow(() -> function.apply("hello", 5)));
+		assertFalse(assertDoesNotThrow(() -> function.apply("hi", 5)));
+	}
+	
+	@Test
+	void applyReturnsNullResult() {
+		ThrowableBiFunction<String, String, String, Exception> function = (a, b) -> null;
+		
+		assertNull(assertDoesNotThrow(() -> function.apply("a", "b")));
+	}
+	
+	@Test
+	void andThenCombinesFunctions() {
 		ThrowableBiFunction<Integer, Integer, Integer, Exception> function = Integer::sum;
-		assertDoesNotThrow(() -> function.andThen(i -> i * 2));
-		assertEquals(4, assertDoesNotThrow(() -> function.andThen(i -> i * 2).apply(1, 1)));
+		ThrowableFunction<Integer, Integer, Exception> after = i -> i * 2;
+		
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> combined = function.andThen(after);
+		
+		assertEquals(10, assertDoesNotThrow(() -> combined.apply(2, 3)));
+		assertEquals(0, assertDoesNotThrow(() -> combined.apply(0, 0)));
+	}
+	
+	@Test
+	void andThenWithDifferentReturnType() {
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> function = Integer::sum;
+		ThrowableFunction<Integer, String, Exception> after = i -> "Result: " + i;
+		
+		ThrowableBiFunction<Integer, Integer, String, Exception> combined = function.andThen(after);
+		
+		assertEquals("Result: 7", assertDoesNotThrow(() -> combined.apply(3, 4)));
+	}
+	
+	@Test
+	void andThenWithMultipleChaining() {
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> function = Integer::sum;
+		ThrowableFunction<Integer, Integer, Exception> multiply = i -> i * 2;
+		ThrowableFunction<Integer, String, Exception> toString = i -> "Final: " + i;
+		
+		ThrowableBiFunction<Integer, Integer, String, Exception> combined = function.andThen(multiply).andThen(toString);
+		
+		assertEquals("Final: 10", assertDoesNotThrow(() -> combined.apply(2, 3)));
+	}
+	
+	@Test
+	void andThenWithNullAfterFunction() {
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> function = Integer::sum;
+		
 		assertThrows(NullPointerException.class, () -> function.andThen(null));
+	}
+	
+	@Test
+	void andThenWithExceptionInOriginalFunction() {
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> function = (a, b) -> {
+			throw new Exception("Original function failed");
+		};
+		ThrowableFunction<Integer, Integer, Exception> after = i -> i * 2;
+		
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> combined = function.andThen(after);
+		
+		Exception exception = assertThrows(Exception.class, () -> combined.apply(1, 2));
+		assertEquals("Original function failed", exception.getMessage());
+	}
+	
+	@Test
+	void andThenWithExceptionInAfterFunction() {
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> function = Integer::sum;
+		ThrowableFunction<Integer, Integer, Exception> after = i -> {
+			throw new Exception("After function failed");
+		};
+		
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> combined = function.andThen(after);
+		
+		Exception exception = assertThrows(Exception.class, () -> combined.apply(1, 2));
+		assertEquals("After function failed", exception.getMessage());
+	}
+	
+	@Test
+	void andThenWithNullResult() {
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> function = (a, b) -> null;
+		ThrowableFunction<Integer, String, Exception> after = i -> i == null ? "null" : i.toString();
+		
+		ThrowableBiFunction<Integer, Integer, String, Exception> combined = function.andThen(after);
+		
+		assertEquals("null", assertDoesNotThrow(() -> combined.apply(1, 2)));
+	}
+	
+	@Test
+	void andThenWithIdentityFunction() {
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> function = Integer::sum;
+		ThrowableFunction<Integer, Integer, Exception> identity = i -> i;
+		
+		ThrowableBiFunction<Integer, Integer, Integer, Exception> combined = function.andThen(identity);
+		
+		assertEquals(5, assertDoesNotThrow(() -> combined.apply(2, 3)));
 	}
 }
--- src/test/java/net/luis/utils/function/throwable/ThrowableFunctionTest.java
+++ src/test/java/net/luis/utils/function/throwable/ThrowableFunctionTest.java
@@ -20,6 +20,8 @@ package net.luis.utils.function.throwable;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Function;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -30,37 +32,193 @@ import static org.junit.jupiter.api.Assertions.*;
 class ThrowableFunctionTest {
 	
 	@Test
-	void caught() {
-		ThrowableFunction<Integer, Integer, Exception> function = (a) -> {
-			if (a != 1) {
-				throw new Exception();
-			}
-			assertEquals(1, a);
-			return a;
+	void caughtConvertsSuccessfulFunction() {
+		ThrowableFunction<Integer, Integer, Exception> function = i -> i * 2;
+		Function<Integer, Integer> caught = ThrowableFunction.caught(function);
+		
+		assertEquals(10, caught.apply(5));
+		assertEquals(0, caught.apply(0));
+		assertEquals(-6, caught.apply(-3));
+	}
+	
+	@Test
+	void caughtWrapsExceptionInRuntimeException() {
+		ThrowableFunction<Integer, Integer, Exception> function = i -> {
+			throw new Exception("Test exception");
 		};
-		assertEquals(1, assertDoesNotThrow(() -> ThrowableFunction.caught(function).apply(1)));
-		assertThrows(RuntimeException.class, () -> ThrowableFunction.caught(function).apply(2));
+		Function<Integer, Integer> caught = ThrowableFunction.caught(function);
+		
+		RuntimeException exception = assertThrows(RuntimeException.class, () -> caught.apply(1));
+		assertInstanceOf(Exception.class, exception.getCause());
+		assertEquals("Test exception", exception.getCause().getMessage());
+	}
+	
+	@Test
+	void caughtWithNullFunction() {
 		assertThrows(NullPointerException.class, () -> ThrowableFunction.caught(null));
 	}
 	
 	@Test
-	void apply() {
-		ThrowableFunction<Integer, Integer, Exception> function = (a) -> {
-			if (a != 1) {
-				throw new Exception();
-			}
-			assertEquals(1, a);
-			return a;
-		};
-		assertEquals(1, assertDoesNotThrow(() -> function.apply(1)));
-		assertThrows(Exception.class, () -> function.apply(2));
+	void caughtWithDifferentParameterTypes() {
+		ThrowableFunction<String, Integer, Exception> function = String::length;
+		Function<String, Integer> caught = ThrowableFunction.caught(function);
+		
+		assertEquals(5, caught.apply("hello"));
+		assertEquals(0, caught.apply(""));
 	}
 	
 	@Test
-	void andThen() {
-		ThrowableFunction<Integer, Integer, Exception> function = (a) -> a;
-		assertDoesNotThrow(() -> function.andThen(i -> i * 2));
-		assertEquals(2, assertDoesNotThrow(() -> function.andThen(i -> i * 2).apply(1)));
+	void caughtWithNullParameter() {
+		ThrowableFunction<String, String, Exception> function = s -> s == null ? "null" : s.toUpperCase();
+		Function<String, String> caught = ThrowableFunction.caught(function);
+		
+		assertEquals("null", caught.apply(null));
+		assertEquals("HELLO", caught.apply("hello"));
+	}
+	
+	@Test
+	void caughtWithNullResult() {
+		ThrowableFunction<String, String, Exception> function = s -> null;
+		Function<String, String> caught = ThrowableFunction.caught(function);
+		
+		assertNull(caught.apply("test"));
+	}
+	
+	@Test
+	void applyWithSuccessfulExecution() {
+		ThrowableFunction<Integer, Integer, Exception> function = i -> i * i;
+		
+		assertEquals(25, assertDoesNotThrow(() -> function.apply(5)));
+		assertEquals(0, assertDoesNotThrow(() -> function.apply(0)));
+		assertEquals(9, assertDoesNotThrow(() -> function.apply(-3)));
+	}
+	
+	@Test
+	void applyWithException() {
+		ThrowableFunction<Integer, Integer, Exception> function = i -> {
+			if (i < 0) {
+				throw new Exception("Negative values not allowed");
+			}
+			return i * 2;
+		};
+		
+		assertEquals(10, assertDoesNotThrow(() -> function.apply(5)));
+		assertEquals(0, assertDoesNotThrow(() -> function.apply(0)));
+		
+		Exception exception = assertThrows(Exception.class, () -> function.apply(-1));
+		assertEquals("Negative values not allowed", exception.getMessage());
+	}
+	
+	@Test
+	void applyWithNullParameter() {
+		ThrowableFunction<String, String, Exception> function = s -> {
+			if (s == null) {
+				throw new Exception("Null parameter not allowed");
+			}
+			return s.toUpperCase();
+		};
+		
+		assertEquals("HELLO", assertDoesNotThrow(() -> function.apply("hello")));
+		assertThrows(Exception.class, () -> function.apply(null));
+	}
+	
+	@Test
+	void applyWithDifferentTypes() {
+		ThrowableFunction<String, Boolean, Exception> function = s -> s.length() > 3;
+		
+		assertTrue(assertDoesNotThrow(() -> function.apply("hello")));
+		assertFalse(assertDoesNotThrow(() -> function.apply("hi")));
+	}
+	
+	@Test
+	void applyReturnsNullResult() {
+		ThrowableFunction<String, String, Exception> function = s -> null;
+		
+		assertNull(assertDoesNotThrow(() -> function.apply("test")));
+	}
+	
+	@Test
+	void andThenCombinesFunctions() {
+		ThrowableFunction<Integer, Integer, Exception> function = i -> i * 2;
+		ThrowableFunction<Integer, Integer, Exception> after = i -> i + 1;
+		
+		ThrowableFunction<Integer, Integer, Exception> combined = function.andThen(after);
+		
+		assertEquals(11, assertDoesNotThrow(() -> combined.apply(5)));
+		assertEquals(1, assertDoesNotThrow(() -> combined.apply(0)));
+	}
+	
+	@Test
+	void andThenWithDifferentReturnType() {
+		ThrowableFunction<Integer, Integer, Exception> function = i -> i * 2;
+		ThrowableFunction<Integer, String, Exception> after = i -> "Result: " + i;
+		
+		ThrowableFunction<Integer, String, Exception> combined = function.andThen(after);
+		
+		assertEquals("Result: 10", assertDoesNotThrow(() -> combined.apply(5)));
+	}
+	
+	@Test
+	void andThenWithMultipleChaining() {
+		ThrowableFunction<Integer, Integer, Exception> function = i -> i + 1;
+		ThrowableFunction<Integer, Integer, Exception> multiply = i -> i * 2;
+		ThrowableFunction<Integer, String, Exception> toString = i -> "Final: " + i;
+		
+		ThrowableFunction<Integer, String, Exception> combined = function.andThen(multiply).andThen(toString);
+		
+		assertEquals("Final: 12", assertDoesNotThrow(() -> combined.apply(5)));
+	}
+	
+	@Test
+	void andThenWithNullAfterFunction() {
+		ThrowableFunction<Integer, Integer, Exception> function = i -> i * 2;
+		
 		assertThrows(NullPointerException.class, () -> function.andThen(null));
+	}
+	
+	@Test
+	void andThenWithExceptionInOriginalFunction() {
+		ThrowableFunction<Integer, Integer, Exception> function = i -> {
+			throw new Exception("Original function failed");
+		};
+		ThrowableFunction<Integer, Integer, Exception> after = i -> i * 2;
+		
+		ThrowableFunction<Integer, Integer, Exception> combined = function.andThen(after);
+		
+		Exception exception = assertThrows(Exception.class, () -> combined.apply(1));
+		assertEquals("Original function failed", exception.getMessage());
+	}
+	
+	@Test
+	void andThenWithExceptionInAfterFunction() {
+		ThrowableFunction<Integer, Integer, Exception> function = i -> i * 2;
+		ThrowableFunction<Integer, Integer, Exception> after = i -> {
+			throw new Exception("After function failed");
+		};
+		
+		ThrowableFunction<Integer, Integer, Exception> combined = function.andThen(after);
+		
+		Exception exception = assertThrows(Exception.class, () -> combined.apply(1));
+		assertEquals("After function failed", exception.getMessage());
+	}
+	
+	@Test
+	void andThenWithNullResult() {
+		ThrowableFunction<Integer, Integer, Exception> function = i -> null;
+		ThrowableFunction<Integer, String, Exception> after = i -> i == null ? "null" : i.toString();
+		
+		ThrowableFunction<Integer, String, Exception> combined = function.andThen(after);
+		
+		assertEquals("null", assertDoesNotThrow(() -> combined.apply(1)));
+	}
+	
+	@Test
+	void andThenWithIdentityFunction() {
+		ThrowableFunction<Integer, Integer, Exception> function = i -> i * 2;
+		ThrowableFunction<Integer, Integer, Exception> identity = i -> i;
+		
+		ThrowableFunction<Integer, Integer, Exception> combined = function.andThen(identity);
+		
+		assertEquals(10, assertDoesNotThrow(() -> combined.apply(5)));
 	}
 }
