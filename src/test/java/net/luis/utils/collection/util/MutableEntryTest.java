5801d6c5e377fe45a62c00f85b4968ceab06fc75
Luis <luis@staudt-fridingen.de>
2025-07-21 23:59:21 +0200

    improved entry and cell tests

--- src/test/java/net/luis/utils/collection/util/MutableEntryTest.java
+++ src/test/java/net/luis/utils/collection/util/MutableEntryTest.java
@@ -30,17 +30,152 @@ import static org.junit.jupiter.api.Assertions.*;
 class MutableEntryTest {
 	
 	@Test
-	void constructor() {
+	void constructorWithNullKey() {
 		assertThrows(NullPointerException.class, () -> new MutableEntry<>(null, "value"));
-		assertDoesNotThrow(() -> new MutableEntry<>("key", "value"));
-		assertDoesNotThrow(() -> new MutableEntry<>("key", null));
+		assertThrows(NullPointerException.class, () -> new MutableEntry<>(null, null));
 	}
 	
 	@Test
-	void setValue() {
-		MutableEntry<String, String> entry = new MutableEntry<>("key", "value0");
-		assertEquals("value0", entry.getValue());
-		assertDoesNotThrow(() -> entry.setValue("value1"));
-		assertEquals("value1", entry.getValue());
+	void constructorWithValidInputs() {
+		assertDoesNotThrow(() -> new MutableEntry<>("key", "value"));
+		assertDoesNotThrow(() -> new MutableEntry<>("key", null));
+		assertDoesNotThrow(() -> new MutableEntry<>(123, "value"));
+		assertDoesNotThrow(() -> new MutableEntry<>("", ""));
+	}
+	
+	@Test
+	void getKey() {
+		MutableEntry<String, String> entry = new MutableEntry<>("testKey", "value");
+		assertEquals("testKey", entry.getKey());
+		
+		MutableEntry<Integer, String> intEntry = new MutableEntry<>(42, "value");
+		assertEquals(42, intEntry.getKey());
+	}
+	
+	@Test
+	void getValueInitial() {
+		MutableEntry<String, String> entry = new MutableEntry<>("key", "initialValue");
+		assertEquals("initialValue", entry.getValue());
+		
+		MutableEntry<String, String> nullEntry = new MutableEntry<>("key", null);
+		assertNull(nullEntry.getValue());
+	}
+	
+	@Test
+	void setValueReturnsOldValue() {
+		MutableEntry<String, String> entry = new MutableEntry<>("key", "oldValue");
+		
+		String oldValue = entry.setValue("newValue");
+		assertEquals("oldValue", oldValue);
+		assertEquals("newValue", entry.getValue());
+	}
+	
+	@Test
+	void setValueWithNull() {
+		MutableEntry<String, String> entry = new MutableEntry<>("key", "value");
+		
+		String oldValue = entry.setValue(null);
+		assertEquals("value", oldValue);
+		assertNull(entry.getValue());
+	}
+	
+	@Test
+	void setValueFromNull() {
+		MutableEntry<String, String> entry = new MutableEntry<>("key", null);
+		
+		String oldValue = entry.setValue("newValue");
+		assertNull(oldValue);
+		assertEquals("newValue", entry.getValue());
+	}
+	
+	@Test
+	void setValueMultipleTimes() {
+		MutableEntry<String, Integer> entry = new MutableEntry<>("key", 1);
+		
+		assertEquals(1, entry.setValue(2));
+		assertEquals(2, entry.getValue());
+		
+		assertEquals(2, entry.setValue(3));
+		assertEquals(3, entry.getValue());
+		
+		assertEquals(3, entry.setValue(null));
+		assertNull(entry.getValue());
+	}
+	
+	@Test
+	void toStringMethod() {
+		MutableEntry<String, String> entry = new MutableEntry<>("key", "value");
+		assertEquals("key=value", entry.toString());
+		
+		MutableEntry<String, String> nullValueEntry = new MutableEntry<>("key", null);
+		assertEquals("key=null", nullValueEntry.toString());
+		
+		MutableEntry<Integer, String> intKeyEntry = new MutableEntry<>(42, "value");
+		assertEquals("42=value", intKeyEntry.toString());
+	}
+	
+	@Test
+	void equalsAndHashCodeSameObjects() {
+		MutableEntry<String, String> entry1 = new MutableEntry<>("key", "value");
+		MutableEntry<String, String> entry2 = new MutableEntry<>("key", "value");
+		
+		assertEquals(entry1, entry2);
+		assertEquals(entry1.hashCode(), entry2.hashCode());
+	}
+	
+	@Test
+	void equalsSameInstance() {
+		MutableEntry<String, String> entry = new MutableEntry<>("key", "value");
+		assertEquals(entry, entry);
+	}
+	
+	@Test
+	void equalsWithDifferentValues() {
+		MutableEntry<String, String> entry1 = new MutableEntry<>("key", "value1");
+		MutableEntry<String, String> entry2 = new MutableEntry<>("key", "value2");
+		
+		assertNotEquals(entry1, entry2);
+	}
+	
+	@Test
+	void equalsWithDifferentKeys() {
+		MutableEntry<String, String> entry1 = new MutableEntry<>("key1", "value");
+		MutableEntry<String, String> entry2 = new MutableEntry<>("key2", "value");
+		
+		assertNotEquals(entry1, entry2);
+	}
+	
+	@Test
+	void equalsWithNull() {
+		MutableEntry<String, String> entry = new MutableEntry<>("key", "value");
+		assertNotEquals(entry, null);
+	}
+	
+	@Test
+	void equalsWithDifferentType() {
+		MutableEntry<String, String> entry = new MutableEntry<>("key", "value");
+		assertNotEquals(entry, "not an entry");
+	}
+	
+	@Test
+	void equalsWithNullValues() {
+		MutableEntry<String, String> entry1 = new MutableEntry<>("key", null);
+		MutableEntry<String, String> entry2 = new MutableEntry<>("key", null);
+		
+		assertEquals(entry1, entry2);
+	}
+	
+	@Test
+	void mutabilityAfterCreation() {
+		MutableEntry<String, String> entry = new MutableEntry<>("key", "initial");
+		
+		MutableEntry<String, String> other = new MutableEntry<>("key", "initial");
+		assertEquals(entry, other);
+		
+		entry.setValue("changed");
+		assertNotEquals(entry, other);
+		
+		other.setValue("changed");
+		assertEquals(entry, other);
 	}
 }
--- src/test/java/net/luis/utils/collection/util/SimpleCellTest.java
+++ src/test/java/net/luis/utils/collection/util/SimpleCellTest.java
@@ -30,26 +30,150 @@ import static org.junit.jupiter.api.Assertions.*;
 class SimpleCellTest {
 	
 	@Test
-	void constructor() {
-		assertThrows(NullPointerException.class, () -> new SimpleCell<>(null, null, "value"));
+	void constructorWithNullRowKey() {
 		assertThrows(NullPointerException.class, () -> new SimpleCell<>(null, "columnKey", "value"));
+		assertThrows(NullPointerException.class, () -> new SimpleCell<>(null, "columnKey", null));
+	}
+	
+	@Test
+	void constructorWithNullColumnKey() {
 		assertThrows(NullPointerException.class, () -> new SimpleCell<>("rowKey", null, "value"));
-		assertDoesNotThrow(() -> new SimpleCell<>("rowKey", "columnKey", null));
+		assertThrows(NullPointerException.class, () -> new SimpleCell<>("rowKey", null, null));
+	}
+	
+	@Test
+	void constructorWithBothKeysNull() {
+		assertThrows(NullPointerException.class, () -> new SimpleCell<>(null, null, "value"));
+		assertThrows(NullPointerException.class, () -> new SimpleCell<>(null, null, null));
+	}
+	
+	@Test
+	void constructorWithValidInputs() {
 		assertDoesNotThrow(() -> new SimpleCell<>("rowKey", "columnKey", "value"));
+		assertDoesNotThrow(() -> new SimpleCell<>("rowKey", "columnKey", null));
+		assertDoesNotThrow(() -> new SimpleCell<>(1, 2, 3));
+		assertDoesNotThrow(() -> new SimpleCell<>("", "", ""));
 	}
 	
 	@Test
 	void getRowKey() {
-		assertEquals("rowKey", new SimpleCell<>("rowKey", "columnKey", "value").getRowKey());
+		SimpleCell<String, String, String> cell = new SimpleCell<>("testRow", "testColumn", "testValue");
+		assertEquals("testRow", cell.getRowKey());
+		
+		SimpleCell<Integer, String, String> intCell = new SimpleCell<>(42, "column", "value");
+		assertEquals(42, intCell.getRowKey());
 	}
 	
 	@Test
 	void getColumnKey() {
-		assertEquals("columnKey", new SimpleCell<>("rowKey", "columnKey", "value").getColumnKey());
+		SimpleCell<String, String, String> cell = new SimpleCell<>("testRow", "testColumn", "testValue");
+		assertEquals("testColumn", cell.getColumnKey());
+		
+		SimpleCell<String, Integer, String> intCell = new SimpleCell<>("row", 42, "value");
+		assertEquals(42, intCell.getColumnKey());
 	}
 	
 	@Test
 	void getValue() {
-		assertEquals("value", new SimpleCell<>("rowKey", "columnKey", "value").getValue());
+		SimpleCell<String, String, String> cell = new SimpleCell<>("row", "column", "testValue");
+		assertEquals("testValue", cell.getValue());
+		
+		SimpleCell<String, String, Integer> intCell = new SimpleCell<>("row", "column", 42);
+		assertEquals(42, intCell.getValue());
+	}
+	
+	@Test
+	void getValueWithNull() {
+		SimpleCell<String, String, String> cell = new SimpleCell<>("row", "column", null);
+		assertNull(cell.getValue());
+	}
+	
+	@Test
+	void toStringMethod() {
+		SimpleCell<String, String, String> cell = new SimpleCell<>("row", "col", "val");
+		assertEquals("(row,col)=val", cell.toString());
+		
+		SimpleCell<String, String, String> nullCell = new SimpleCell<>("row", "col", null);
+		assertEquals("(row,col)=null", nullCell.toString());
+		
+		SimpleCell<Integer, Integer, String> intCell = new SimpleCell<>(1, 2, "value");
+		assertEquals("(1,2)=value", intCell.toString());
+	}
+	
+	@Test
+	void equalsAndHashCodeSameObjects() {
+		SimpleCell<String, String, String> cell1 = new SimpleCell<>("row", "col", "val");
+		SimpleCell<String, String, String> cell2 = new SimpleCell<>("row", "col", "val");
+		
+		assertEquals(cell1, cell2);
+		assertEquals(cell1.hashCode(), cell2.hashCode());
+	}
+	
+	@Test
+	void equalsSameInstance() {
+		SimpleCell<String, String, String> cell = new SimpleCell<>("row", "col", "val");
+		assertEquals(cell, cell);
+	}
+	
+	@Test
+	void equalsWithDifferentRowKey() {
+		SimpleCell<String, String, String> cell1 = new SimpleCell<>("row1", "col", "val");
+		SimpleCell<String, String, String> cell2 = new SimpleCell<>("row2", "col", "val");
+		
+		assertNotEquals(cell1, cell2);
+	}
+	
+	@Test
+	void equalsWithDifferentColumnKey() {
+		SimpleCell<String, String, String> cell1 = new SimpleCell<>("row", "col1", "val");
+		SimpleCell<String, String, String> cell2 = new SimpleCell<>("row", "col2", "val");
+		
+		assertNotEquals(cell1, cell2);
+	}
+	
+	@Test
+	void equalsWithDifferentValue() {
+		SimpleCell<String, String, String> cell1 = new SimpleCell<>("row", "col", "val1");
+		SimpleCell<String, String, String> cell2 = new SimpleCell<>("row", "col", "val2");
+		
+		assertNotEquals(cell1, cell2);
+	}
+	
+	@Test
+	void equalsWithNull() {
+		SimpleCell<String, String, String> cell = new SimpleCell<>("row", "col", "val");
+		assertNotEquals(cell, null);
+	}
+	
+	@Test
+	void equalsWithDifferentType() {
+		SimpleCell<String, String, String> cell = new SimpleCell<>("row", "col", "val");
+		assertNotEquals(cell, "not a cell");
+	}
+	
+	@Test
+	void equalsWithNullValues() {
+		SimpleCell<String, String, String> cell1 = new SimpleCell<>("row", "col", null);
+		SimpleCell<String, String, String> cell2 = new SimpleCell<>("row", "col", null);
+		
+		assertEquals(cell1, cell2);
+	}
+	
+	@Test
+	void equalsWithMixedNullValues() {
+		SimpleCell<String, String, String> cell1 = new SimpleCell<>("row", "col", null);
+		SimpleCell<String, String, String> cell2 = new SimpleCell<>("row", "col", "value");
+		
+		assertNotEquals(cell1, cell2);
+	}
+	
+	@Test
+	void differentTypes() {
+		SimpleCell<String, Integer, Boolean> mixedCell = new SimpleCell<>("row", 42, true);
+		
+		assertEquals("row", mixedCell.getRowKey());
+		assertEquals(42, mixedCell.getColumnKey());
+		assertEquals(true, mixedCell.getValue());
+		assertEquals("(row,42)=true", mixedCell.toString());
 	}
 }
