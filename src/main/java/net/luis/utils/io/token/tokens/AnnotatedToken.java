2814d8956c8f9b79617da70a0e937687e44acff8
Luis <luis@staudt-fridingen.de>
2025-08-03 17:56:52 +0200

    added annotated and indexed token implementation

---
+++ src/main/java/net/luis/utils/io/token/tokens/AnnotatedToken.java
@@ -0,0 +1,140 @@
+/*
+ * LUtils
+ * Copyright (C) 2025 Luis Staudt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.luis.utils.io.token.tokens;
+
+import net.luis.utils.io.token.TokenPosition;
+import net.luis.utils.io.token.definition.TokenDefinition;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnknownNullability;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Token implementation that wraps another token with additional metadata.<br>
+ * This token preserves all properties of the wrapped token while adding metadata support.<br>
+ * The metadata can be used to store additional information about the token during processing.<br>
+ *
+ * @author Luis-St
+ *
+ * @param token The wrapped token
+ * @param metadata The metadata associated with the token
+ */
+public record AnnotatedToken(
+	@NotNull Token token,
+	@NotNull Map<String, Object> metadata
+) implements Token {
+	
+	/**
+	 * Constructs a new annotated token with the given token and metadata.<br>
+	 *
+	 * @param token The wrapped token
+	 * @param metadata The metadata associated with the token
+	 * @throws NullPointerException If the token or metadata map is null
+	 */
+	public AnnotatedToken {
+		Objects.requireNonNull(token, "Token must not be null");
+		Objects.requireNonNull(metadata, "Metadata map must not be null");
+		metadata = Map.copyOf(metadata);
+	}
+	
+	/**
+	 * Creates an annotated token with a single metadata entry.<br>
+	 *
+	 * @param token The wrapped token
+	 * @param key The metadata key
+	 * @param value The metadata value
+	 * @return A new annotated token
+	 * @throws NullPointerException If the token or key is null
+	 */
+	public static @NotNull AnnotatedToken of(@NotNull Token token, @NotNull String key, Object value) {
+		Objects.requireNonNull(key, "Metadata key must not be null");
+		return new AnnotatedToken(token, Map.of(key, value));
+	}
+	
+	/**
+	 * Creates an annotated token with empty metadata.<br>
+	 *
+	 * @param token The wrapped token
+	 * @return A new annotated token with empty metadata
+	 * @throws NullPointerException If the token is null
+	 */
+	public static @NotNull AnnotatedToken empty(@NotNull Token token) {
+		return new AnnotatedToken(token, Map.of());
+	}
+	
+	@Override
+	public @NotNull TokenDefinition definition() {
+		return this.token.definition();
+	}
+	
+	@Override
+	public @NotNull String value() {
+		return this.token.value();
+	}
+	
+	@Override
+	public @NotNull TokenPosition startPosition() {
+		return this.token.startPosition();
+	}
+	
+	@Override
+	public @NotNull TokenPosition endPosition() {
+		return this.token.endPosition();
+	}
+	
+	/**
+	 * Gets a metadata value by key.<br>
+	 *
+	 * @param key The metadata key
+	 * @return The metadata value, or null if not present
+	 * @throws NullPointerException If the key is null
+	 */
+	public @UnknownNullability Object getMetadata(@NotNull String key) {
+		Objects.requireNonNull(key, "Metadata key must not be null");
+		return this.metadata.get(key);
+	}
+	
+	/**
+	 * Gets a metadata value by key with a default value.<br>
+	 *
+	 * @param key The metadata key
+	 * @param defaultValue The default value if key is not present
+	 * @param <T> The type of the metadata value
+	 * @return The metadata value, or the default value if not present
+	 * @throws NullPointerException If the key is null
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> @UnknownNullability T getMetadata(@NotNull String key, T defaultValue) {
+		Objects.requireNonNull(key, "Metadata key must not be null");
+		return (T) this.metadata.getOrDefault(key, defaultValue);
+	}
+	
+	/**
+	 * Checks if the metadata contains the given key.<br>
+	 *
+	 * @param key The metadata key to check
+	 * @return True if the metadata contains the key, false otherwise
+	 * @throws NullPointerException If the key is null
+	 */
+	public boolean hasMetadata(@NotNull String key) {
+		Objects.requireNonNull(key, "Metadata key must not be null");
+		return this.metadata.containsKey(key);
+	}
+}
---
+++ src/test/java/net/luis/utils/io/token/tokens/AnnotatedTokenTest.java
@@ -0,0 +1,330 @@
+/*
+ * LUtils
+ * Copyright (C) 2025 Luis Staudt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.luis.utils.io.token.tokens;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for {@link AnnotatedToken}.<br>
+ *
+ * @author Luis-St
+ */
+class AnnotatedTokenTest {
+	
+	private static @NotNull Token createToken(@NotNull String value) {
+		return SimpleToken.createUnpositioned(word -> word.equals(value), value);
+	}
+	
+	@Test
+	void constructorWithNullToken() {
+		Map<String, Object> metadata = Map.of("key", "value");
+		
+		assertThrows(NullPointerException.class, () -> new AnnotatedToken(null, metadata));
+	}
+	
+	@Test
+	void constructorWithNullMetadata() {
+		Token token = createToken("test");
+		
+		assertThrows(NullPointerException.class, () -> new AnnotatedToken(token, null));
+	}
+	
+	@Test
+	void constructorWithNullValue() {
+		Token token = createToken("test");
+		Map<String, Object> metadata = new HashMap<>();
+		metadata.put("nullable", null);
+		
+		assertThrows(NullPointerException.class, () -> new AnnotatedToken(token, metadata));
+	}
+	
+	@Test
+	void constructorWithValidParameters() {
+		Token token = createToken("test");
+		Map<String, Object> metadata = Map.of("key", "value");
+		
+		assertDoesNotThrow(() -> new AnnotatedToken(token, metadata));
+	}
+	
+	@Test
+	void constructorWithEmptyMetadata() {
+		Token token = createToken("test");
+		Map<String, Object> emptyMetadata = Map.of();
+		
+		assertDoesNotThrow(() -> new AnnotatedToken(token, emptyMetadata));
+	}
+	
+	@Test
+	void constructorMakesImmutableCopyOfMetadata() {
+		Token token = createToken("test");
+		Map<String, Object> mutableMetadata = new HashMap<>();
+		mutableMetadata.put("key", "value");
+		
+		AnnotatedToken annotatedToken = new AnnotatedToken(token, mutableMetadata);
+		mutableMetadata.put("newKey", "newValue");
+		
+		assertFalse(annotatedToken.metadata().containsKey("newKey"));
+		assertThrows(UnsupportedOperationException.class, () -> annotatedToken.metadata().put("test", "test"));
+	}
+	
+	@Test
+	void ofWithNullToken() {
+		assertThrows(NullPointerException.class, () -> AnnotatedToken.of(null, "key", "value"));
+	}
+	
+	@Test
+	void ofWithNullKey() {
+		Token token = createToken("test");
+		
+		assertThrows(NullPointerException.class, () -> AnnotatedToken.of(token, null, "value"));
+	}
+	
+	@Test
+	void ofWithValidParameters() {
+		Token token = createToken("test");
+		
+		AnnotatedToken annotated = AnnotatedToken.of(token, "type", "identifier");
+		
+		assertEquals(token, annotated.token());
+		assertEquals(1, annotated.metadata().size());
+		assertEquals("identifier", annotated.metadata().get("type"));
+	}
+	
+	@Test
+	void ofWithNullValue() {
+		Token token = createToken("test");
+		
+		assertThrows(NullPointerException.class, () -> AnnotatedToken.of(token, "nullable", null));
+	}
+	
+	@Test
+	void emptyWithNullToken() {
+		assertThrows(NullPointerException.class, () -> AnnotatedToken.empty(null));
+	}
+	
+	@Test
+	void emptyWithValidToken() {
+		Token token = createToken("test");
+		
+		AnnotatedToken annotated = AnnotatedToken.empty(token);
+		
+		assertEquals(token, annotated.token());
+		assertTrue(annotated.metadata().isEmpty());
+	}
+	
+	@Test
+	void tokenReturnsWrappedToken() {
+		Token originalToken = createToken("original");
+		Map<String, Object> metadata = Map.of("key", "value");
+		AnnotatedToken annotated = new AnnotatedToken(originalToken, metadata);
+		
+		assertSame(originalToken, annotated.token());
+	}
+	
+	@Test
+	void metadataReturnsCorrectMap() {
+		Token token = createToken("test");
+		Map<String, Object> originalMetadata = Map.of("type", "keyword", "line", 5);
+		AnnotatedToken annotated = new AnnotatedToken(token, originalMetadata);
+		
+		assertEquals(originalMetadata, annotated.metadata());
+	}
+	
+	@Test
+	void definitionDelegatesToWrappedToken() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.empty(token);
+		
+		assertEquals(token.definition(), annotated.definition());
+	}
+	
+	@Test
+	void valueDelegatesToWrappedToken() {
+		Token token = createToken("testValue");
+		AnnotatedToken annotated = AnnotatedToken.empty(token);
+		
+		assertEquals("testValue", annotated.value());
+	}
+	
+	@Test
+	void startPositionDelegatesToWrappedToken() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.empty(token);
+		
+		assertEquals(token.startPosition(), annotated.startPosition());
+	}
+	
+	@Test
+	void endPositionDelegatesToWrappedToken() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.empty(token);
+		
+		assertEquals(token.endPosition(), annotated.endPosition());
+	}
+	
+	@Test
+	void getMetadataWithNullKey() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.of(token, "key", "value");
+		
+		assertThrows(NullPointerException.class, () -> annotated.getMetadata(null));
+	}
+	
+	@Test
+	void getMetadataWithExistingKey() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.of(token, "type", "identifier");
+		
+		assertEquals("identifier", annotated.getMetadata("type"));
+	}
+	
+	@Test
+	void getMetadataWithNonExistingKey() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.of(token, "type", "identifier");
+		
+		assertNull(annotated.getMetadata("nonExisting"));
+	}
+	
+	@Test
+	void getMetadataWithDefaultValueAndNullKey() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.empty(token);
+		
+		assertThrows(NullPointerException.class, () -> annotated.getMetadata(null, "default"));
+	}
+	
+	@Test
+	void getMetadataWithDefaultValueAndExistingKey() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.of(token, "count", 42);
+		
+		assertEquals(42, annotated.getMetadata("count", 0));
+	}
+	
+	@Test
+	void getMetadataWithDefaultValueAndNonExistingKey() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.empty(token);
+		
+		assertEquals("default", annotated.getMetadata("missing", "default"));
+	}
+	
+	@Test
+	void hasMetadataWithNullKey() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.of(token, "key", "value");
+		
+		assertThrows(NullPointerException.class, () -> annotated.hasMetadata(null));
+	}
+	
+	@Test
+	void hasMetadataWithExistingKey() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.of(token, "exists", "yes");
+		
+		assertTrue(annotated.hasMetadata("exists"));
+	}
+	
+	@Test
+	void hasMetadataWithNonExistingKey() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.empty(token);
+		
+		assertFalse(annotated.hasMetadata("doesNotExist"));
+	}
+	
+	@Test
+	void metadataWithComplexValues() {
+		Token token = createToken("test");
+		Map<String, Object> complexMetadata = Map.of(
+			"string", "value",
+			"number", 42,
+			"boolean", true,
+			"list", java.util.List.of("a", "b", "c"),
+			"nested", Map.of("inner", "value")
+		);
+		
+		AnnotatedToken annotated = new AnnotatedToken(token, complexMetadata);
+		
+		assertEquals("value", annotated.getMetadata("string"));
+		assertEquals(42, annotated.getMetadata("number"));
+		assertEquals(true, annotated.getMetadata("boolean"));
+		assertEquals(List.of("a", "b", "c"), annotated.getMetadata("list"));
+		assertEquals(Map.of("inner", "value"), annotated.getMetadata("nested"));
+	}
+	
+	@Test
+	void annotatedTokenImplementsTokenInterface() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.empty(token);
+		
+		assertInstanceOf(Token.class, annotated);
+	}
+	
+	@Test
+	void wrappingAnnotatedToken() {
+		Token baseToken = createToken("base");
+		AnnotatedToken firstLevel = AnnotatedToken.of(baseToken, "level", 1);
+		AnnotatedToken secondLevel = AnnotatedToken.of(firstLevel, "level", 2);
+		
+		assertEquals(firstLevel, secondLevel.token());
+		assertEquals(2, secondLevel.getMetadata("level"));
+		assertEquals(baseToken, firstLevel.token());
+		assertEquals(1, firstLevel.getMetadata("level"));
+	}
+	
+	@Test
+	void metadataTypeCasting() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.of(token, "count", 42);
+		
+		Integer count = annotated.getMetadata("count", 0);
+		assertEquals(42, count);
+		
+		String defaultString = annotated.getMetadata("missing", "default");
+		assertEquals("default", defaultString);
+	}
+	
+	@Test
+	void toStringContainsTokenInfo() {
+		Token token = createToken("test");
+		AnnotatedToken annotated = AnnotatedToken.of(token, "type", "test");
+		String tokenString = annotated.toString();
+		
+		assertTrue(tokenString.contains("AnnotatedToken"));
+		assertTrue(tokenString.contains("token"));
+		assertTrue(tokenString.contains("metadata"));
+	}
+	
+	@Test
+	void equalAnnotatedTokensHaveSameHashCode() {
+		Token token = createToken("test");
+		Map<String, Object> metadata = Map.of("key", "value");
+		AnnotatedToken annotated1 = new AnnotatedToken(token, metadata);
+		AnnotatedToken annotated2 = new AnnotatedToken(token, metadata);
+		
+		assertEquals(annotated1.hashCode(), annotated2.hashCode());
+	}
+}
