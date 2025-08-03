4cfaeae3f6553813cbcd266253ceb642b2ff05a2
Luis <luis@staudt-fridingen.de>
2025-08-03 22:25:01 +0200

    added missing tests for array codec

--- src/test/java/net/luis/utils/io/codec/CodecTest.java
+++ src/test/java/net/luis/utils/io/codec/CodecTest.java
@@ -81,6 +81,121 @@ class CodecTest {
 	}
 	
 	@Test
+	void arrayCodecs() {
+		assertThrows(NullPointerException.class, () -> Codec.array(null, INTEGER));
+		assertThrows(NullPointerException.class, () -> Codec.array(Integer.class, null));
+		assertThrows(NullPointerException.class, () -> Codec.noneEmptyArray(null, INTEGER));
+		assertThrows(NullPointerException.class, () -> Codec.noneEmptyArray(Integer.class, null));
+		assertThrows(IllegalArgumentException.class, () -> Codec.array(Integer.class, INTEGER, -1));
+		assertThrows(IllegalArgumentException.class, () -> Codec.array(Integer.class, INTEGER, -1, 2));
+		assertThrows(IllegalArgumentException.class, () -> Codec.array(Integer.class, INTEGER, 2, 1));
+		
+		assertInstanceOf(ArrayCodec.class, Codec.array(Integer.class, INTEGER));
+		assertInstanceOf(ArrayCodec.class, INTEGER.array(Integer.class));
+		assertInstanceOf(ArrayCodec.class, Codec.noneEmptyArray(Integer.class, INTEGER));
+		assertInstanceOf(ArrayCodec.class, INTEGER.noneEmptyArray(Integer.class));
+		
+		assertThrows(NullPointerException.class, () -> INTEGER.array(null));
+		assertThrows(NullPointerException.class, () -> INTEGER.array(null, 5));
+		assertThrows(NullPointerException.class, () -> INTEGER.array(null, 1, 5));
+		assertThrows(NullPointerException.class, () -> INTEGER.noneEmptyArray(null));
+		
+		JsonTypeProvider provider = JsonTypeProvider.INSTANCE;
+		Codec<Integer[]> arrayCodec = INTEGER.array(Integer.class);
+		
+		Integer[] originalArray = new Integer[]{1, 2, 3};
+		JsonElement encoded = arrayCodec.encode(provider, originalArray);
+		assertInstanceOf(JsonArray.class, encoded);
+		assertEquals(3, ((JsonArray) encoded).size());
+		
+		Integer[] decoded = arrayCodec.decode(provider, encoded);
+		assertArrayEquals(originalArray, decoded);
+		
+		Codec<Integer[]> limitedArrayCodec = INTEGER.array(Integer.class, 2);
+		assertTrue(limitedArrayCodec.encodeStart(provider, provider.empty(), new Integer[]{1, 2}).isSuccess());
+		assertTrue(limitedArrayCodec.encodeStart(provider, provider.empty(), new Integer[]{1, 2, 3}).isError());
+		
+		Codec<String[]> nonEmptyArrayCodec = STRING.noneEmptyArray(String.class);
+		assertTrue(nonEmptyArrayCodec.encodeStart(provider, provider.empty(), new String[]{"test"}).isSuccess());
+		assertTrue(nonEmptyArrayCodec.encodeStart(provider, provider.empty(), new String[]{}).isError());
+	}
+	
+	@Test
+	void nestedArrayCodecs() {
+		JsonTypeProvider provider = JsonTypeProvider.INSTANCE;
+		
+		Codec<Integer[][]> twoDArrayCodec = Codec.array(Integer[].class, INTEGER.array(Integer.class));
+		
+		Integer[][] originalArray = new Integer[][]{
+			{1, 2, 3},
+			{4, 5, 6}
+		};
+		
+		JsonElement encoded = twoDArrayCodec.encode(provider, originalArray);
+		assertInstanceOf(JsonArray.class, encoded);
+		
+		Integer[][] decoded = twoDArrayCodec.decode(provider, encoded);
+		assertEquals(2, decoded.length);
+		assertArrayEquals(new Integer[]{1, 2, 3}, decoded[0]);
+		assertArrayEquals(new Integer[]{4, 5, 6}, decoded[1]);
+		
+		Codec<String[][][]> threeDArrayCodec = Codec.array(String[][].class,
+			Codec.array(String[].class, STRING.array(String.class)));
+		
+		String[][][] original3D = new String[][][]{
+			{
+				{"a", "b"},
+				{"c", "d"}
+			},
+			{
+				{"e", "f"},
+				{"g", "h"}
+			}
+		};
+		
+		JsonElement encoded3D = threeDArrayCodec.encode(provider, original3D);
+		String[][][] decoded3D = threeDArrayCodec.decode(provider, encoded3D);
+		
+		assertEquals(2, decoded3D.length);
+		assertEquals(2, decoded3D[0].length);
+		assertArrayEquals(new String[]{"a", "b"}, decoded3D[0][0]);
+		assertArrayEquals(new String[]{"c", "d"}, decoded3D[0][1]);
+		assertArrayEquals(new String[]{"e", "f"}, decoded3D[1][0]);
+		assertArrayEquals(new String[]{"g", "h"}, decoded3D[1][1]);
+	}
+	
+	@Test
+	void arrayCodecsWithConstraints() {
+		JsonTypeProvider provider = JsonTypeProvider.INSTANCE;
+		
+		Codec<Integer[]> constrainedArrayCodec = INTEGER.array(Integer.class, 2, 4);
+		
+		Integer[] validArray = new Integer[]{1, 2, 3};
+		assertTrue(constrainedArrayCodec.encodeStart(provider, provider.empty(), validArray).isSuccess());
+		
+		Integer[] tooSmall = new Integer[]{1};
+		assertTrue(constrainedArrayCodec.encodeStart(provider, provider.empty(), tooSmall).isError());
+		
+		Integer[] tooLarge = new Integer[]{1, 2, 3, 4, 5};
+		assertTrue(constrainedArrayCodec.encodeStart(provider, provider.empty(), tooLarge).isError());
+		
+		Codec<Integer[][]> constrainedNestedCodec = Codec.array(Integer[].class,
+			INTEGER.array(Integer.class, 2, 3), 1, 2);
+		
+		Integer[][] validNested = new Integer[][]{
+			{1, 2, 3},
+			{4, 5}
+		};
+		assertTrue(constrainedNestedCodec.encodeStart(provider, provider.empty(), validNested).isSuccess());
+		
+		Integer[][] invalidNested = new Integer[][]{
+			{1},
+			{2, 3}
+		};
+		assertTrue(constrainedNestedCodec.encodeStart(provider, provider.empty(), invalidNested).isError());
+	}
+	
+	@Test
 	void listCodecs() {
 		assertThrows(NullPointerException.class, () -> Codec.list((Codec<Integer>) null));
 		assertThrows(NullPointerException.class, () -> Codec.noneEmptyList((Codec<Integer>) null));
