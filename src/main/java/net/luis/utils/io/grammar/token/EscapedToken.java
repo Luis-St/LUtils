80d256ccac434b8fd3d65255ff80dd397c278a25
Luis <luis@staudt-fridingen.de>
2026-07-18 00:16:13 +0200

    renamed and updated token system (moved to grammar package)

---
+++ src/main/java/net/luis/utils/io/grammar/token/EscapedToken.java
@@ -0,0 +1,93 @@
+/*
+ * LUtils
+ * Copyright (C) 2026 Luis Staudt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.luis.utils.io.grammar.token;
+
+import com.google.common.collect.Sets;
+import net.luis.utils.io.grammar.token.type.TokenType;
+import org.jspecify.annotations.NonNull;
+
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Special token implementation for escaped tokens.<br>
+ * An escaped token is a token that starts with a backslash and has a length of 2.<br>
+ *
+ * @author Luis-St
+ *
+ * @param value The string value of the token
+ * @param position The position of the token
+ * @param types The token types of the token
+ */
+public record EscapedToken(
+	@NonNull String value,
+	@NonNull TokenPosition position,
+	@NonNull Set<TokenType> types
+) implements Token {
+	
+	/**
+	 * Constructs a new escaped token for a string value without any types.<br>
+	 *
+	 * @param value The string value of the token
+	 * @param position The position of the token
+	 * @throws NullPointerException If the token value or the position is null
+	 * @throws IllegalArgumentException If the token value does not have a length of 2 or does not start with a backslash
+	 */
+	public EscapedToken(@NonNull String value, @NonNull TokenPosition position) {
+		this(value, position, Set.of());
+	}
+	
+	/**
+	 * Constructs a new escaped token for a string value.<br>
+	 *
+	 * @param value The string value of the token
+	 * @param position The position of the token
+	 * @param types The token types of the token
+	 * @throws NullPointerException If the token value, the position, or the types are null
+	 * @throws IllegalArgumentException If the token value does not have a length of 2 or does not start with a backslash
+	 */
+	public EscapedToken {
+		Objects.requireNonNull(value, "Token value must not be null");
+		Objects.requireNonNull(position, "Token position must not be null");
+		Objects.requireNonNull(types, "Token types must not be null");
+		
+		if (value.length() != 2 || value.charAt(0) != '\\') {
+			throw new IllegalArgumentException("Escaped token value '" + value + "' must be of length 2 and start with '\\'");
+		}
+		
+		types = Sets.newHashSet(types);
+	}
+	
+	/**
+	 * Creates an unpositioned escaped token for the given value.<br>
+	 *
+	 * @param value The string value of the token
+	 * @return The unpositioned escaped token
+	 * @throws NullPointerException If the token value is null
+	 * @throws IllegalArgumentException If the token value does not have a length of 2 or does not start with a backslash
+	 */
+	public static @NonNull EscapedToken createUnpositioned(@NonNull String value) {
+		return new EscapedToken(value, TokenPosition.UNPOSITIONED);
+	}
+	
+	@Override
+	public @NonNull String toString() {
+		return "EscapedToken[value=" + this.value.replace("\\", "\\\\") + ",position=" + this.position + "]";
+	}
+}
---
+++ src/main/java/net/luis/utils/io/grammar/token/Token.java
@@ -0,0 +1,119 @@
+/*
+ * LUtils
+ * Copyright (C) 2026 Luis Staudt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.luis.utils.io.grammar.token;
+
+import net.luis.utils.io.grammar.token.type.TokenType;
+import org.jspecify.annotations.NonNull;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Interface for a token, a token is a sequence of characters.<br>
+ *
+ * @author Luis-St
+ */
+public interface Token {
+	
+	/**
+	 * Returns the value of the token.<br>
+	 * @return The value
+	 */
+	@NonNull String value();
+	
+	/**
+	 * Returns the start position of the token.<br>
+	 * The start position is the first character of the token.<br>
+	 *
+	 * @return The start position
+	 */
+	@NonNull TokenPosition position();
+	
+	/**
+	 * Returns the types of the token.<br>
+	 * <p>
+	 *     This set is by default empty and mutable.<br>
+	 *     Types can be added directly to the token by getting the set and adding the type to it:
+	 * </p>
+	 * <pre>{@code
+	 *     token.types().add(myType);
+	 *}</pre>
+	 *
+	 * @return The set of token types
+	 */
+	@NonNull Set<TokenType> types();
+	
+	/**
+	 * Creates an indexed token that wraps this token with the given index.<br>
+	 *
+	 * @param index The index to associate with the token
+	 * @return The indexed token
+	 * @throws IllegalArgumentException If the index is negative
+	 * @apiNote Calling this method is equivalent to {@code new IndexedToken(this, index)} but is more readable.<br>
+	 * If you call this method on an already indexed token, it will create a new indexed token only if the index is different.
+	 * @see IndexedToken#IndexedToken(Token, int)
+	 */
+	default @NonNull Token index(int index) {
+		return new IndexedToken(this, index);
+	}
+	
+	/**
+	 * Creates an annotated token that wraps this token with the given annotations.<br>
+	 * The annotations can be used to store additional information about the token during processing.<br>
+	 *
+	 * @param annotations The annotations to associate with the token
+	 * @return The annotated token
+	 * @throws NullPointerException If the annotations map is null
+	 * @apiNote Calling this method is equivalent to {@code new AnnotatedToken(this, annotations)} but is more readable.<br>
+	 * If you call this method on an already annotated token, it will return itself.
+	 * @see AnnotatedToken#AnnotatedToken(Token, Map)
+	 */
+	default @NonNull Token annotate(@NonNull Map<String, Object> annotations) {
+		return new AnnotatedToken(this, annotations);
+	}
+	
+	/**
+	 * Creates a shadow token that wraps this token.<br>
+	 * A shadow token is a token that does not affect the parsing process.<br>
+	 * It is used to mark tokens that should be ignored during parsing but still need to be retained for other purposes (e.g., comments, whitespace).<br>
+	 * Shadow tokens are typically skipped by parsers and do not contribute to the syntactic structure of the input.<br>
+	 *
+	 * @return The shadow token
+	 * @apiNote Calling this method is equivalent to {@code new ShadowToken(this)} but is more readable.<br>
+	 * If you call this method on an already shadow token, it will return itself.
+	 * @see ShadowToken#ShadowToken(Token)
+	 */
+	default @NonNull Token shadow() {
+		return new ShadowToken(this);
+	}
+	
+	/**
+	 * If this token is a shadow token, it returns the underlying token that it wraps.<br>
+	 * If this token is not a shadow token, it returns itself.<br>
+	 * This method is useful for retrieving the original token when dealing with shadow tokens.<br>
+	 *
+	 * @return The unshadowed token or itself if not a shadow token
+	 * @apiNote Calling this method on a non-shadow token will return itself.<br>
+	 * If you call this method on an already unshadowed token, it will return itself.
+	 * @see ShadowToken#token()
+	 */
+	default @NonNull Token unshadow() {
+		return this;
+	}
+}
